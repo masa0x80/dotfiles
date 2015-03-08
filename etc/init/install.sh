3b6c89904150f1199cdf0be5ca8a526f6dff7c74
masa0x80 <masa0x80@gmail.com>
2015-03-08 21:41:07 +0900

    インストールスクリプトを追加
    - スクリプト内部でOSを判定してhomebrewまたはansibleを実行します

---
+++ etc/init/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mkdir -p $HOME/src
+
+# 実行ファイルのあるディレクトリーを調べる
+abs_dirname() {
+  local CWD="$(pwd)"
+  local FILE_PATH="$1"
+
+  while [ -n "$FILE_PATH" ]; do
+    cd "${FILE_PATH%/*}"
+    local NAME="${FILE_PATH##*/}"
+    FILE_PATH="$(readlink "$NAME" || true)"
+  done
+
+  pwd -P
+  cd "$CWD"
+}
+export SCRIPT_DIR="$(abs_dirname "$0")"
+
+cd $SCRIPT_DIR/..
+case `uname` in
+  Darwin)
+    bash osx/install.sh
+    ;;
+  Linux)
+    bash centos/install.sh
+    cd centos/ansible
+    source $HOME/.bashrc
+    ansible-playbook -i hosts localhost.yml
+    ;;
+esac
+
+unset SCRIPT_DIR
