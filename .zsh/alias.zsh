37d43c01443208d8a985300da29da16de4e3d216
Kimura Masayuki <masa0x80@gmail.com>
2016-01-03 18:57:20 +0900

    Install fzf

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -35,6 +35,29 @@ function git-pull-and-prune() {
 }
 alias ggpull='git-pull-and-prune'
 
+# fshow - git commit browser (enter for show, ctrl-d for diff)
+fshow() {
+  local out shas sha q k
+  while out=$(
+      git log --graph --color=always \
+          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+      fzf --ansi --multi --no-sort --reverse --query="$q" \
+          --print-query --expect=ctrl-d); do
+    q=$(head -1 <<< "$out")
+    k=$(head -2 <<< "$out" | tail -1)
+    shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
+    [ -z "$shas" ] && continue
+    if [ "$k" = ctrl-d ]; then
+      git diff --color=always $shas | less -R
+    else
+      for sha in $shas; do
+        git show --color=always $sha | less -R
+      done
+    fi
+  done
+}
+alias glg='fshow'
+
 # autojumpした時はtmuxのwindow名をディレクトリー名に変更する
 function jump-and-tmux-rename-window() {
   \j "$@"
