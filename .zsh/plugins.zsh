4d603ba4259677e92969d287bf9069f9b397555d
Kimura Masayuki <masa0x80@gmail.com>
2016-01-06 00:40:10 +0900

    ツールの選別

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,16 +1,18 @@
-source $HOME/.zplug/zplug
+if [ -r $HOME/.zplug/zplug ]; then
+  source $HOME/.zplug/zplug
 
-zplug 'plugins/git', from:oh-my-zsh
-zplug 'themes/ys', from:oh-my-zsh
-zplug 'mollifier/anyframe'
-zplug 'b4b4r07/enhancd', of:enhancd.sh
-zplug 'stedolan/jq', as:command, file:jq
+  zplug 'plugins/git', from:oh-my-zsh
+  zplug 'themes/ys', from:oh-my-zsh
+  zplug 'mollifier/anyframe'
+  zplug 'b4b4r07/enhancd', of:enhancd.sh
+  zplug 'stedolan/jq', as:command, file:jq
 
-if ! zplug check --verbose; then
-  printf "Install? [y/N]: "
-  if read -q; then
-    echo; zplug install
+  if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+      echo; zplug install
+    fi
   fi
-fi
 
-zplug load
+  zplug load
+fi
