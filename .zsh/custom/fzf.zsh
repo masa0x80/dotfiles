faff21a74e2b9669d9fe89ebb7e0d5c4c1b79ec5
Kimura Masayuki <masa0x80@gmail.com>
2016-01-06 23:01:43 +0900

    Fix fzf configuration

--- .zsh/custom/fzf.zsh
+++ .zsh/custom/fzf.zsh
@@ -1,7 +1,3 @@
-if type fzf > /dev/null 2>&1; then
-  bindkey '^E' fzf-file-widget
-fi
-
 if [ -r $HOME/.zplug/zplug ]; then
   if type fzf > /dev/null 2>&1; then
     zstyle ":anyframe:selector:" use fzf
@@ -10,38 +6,42 @@ if [ -r $HOME/.zplug/zplug ]; then
   # git branch
   bindkey '^gb'  anyframe-widget-checkout-git-branch
   bindkey '^g^b' anyframe-widget-checkout-git-branch
-  bindkey '^b^b' anyframe-widget-insert-git-branch
+  bindkey '^gB' anyframe-widget-insert-git-branch
 fi
 
-# git-add with peco
-# ref: http://petitviolet.hatenablog.com/entry/20140722/1406034439
-function peco-git-add() {
-  local SELECTED_FILE_TO_ADD="$(git status --porcelain  | \
-                                fzf --query "$LBUFFER" | \
-                                awk -F ' ' '{print $NF}')"
-  if [ -n "$SELECTED_FILE_TO_ADD" ]; then
-    BUFFER="git add $(echo "$SELECTED_FILE_TO_ADD" | tr '\n' ' ')"
-    CURSOR=$#BUFFER
-    zle accept-line
-  fi
-}
-zle -N peco-git-add
-bindkey "^ga"  peco-git-add
-bindkey "^g^a" peco-git-add
+if type fzf > /dev/null 2>&1; then
+  bindkey '^Q' fzf-file-widget
 
-function peco-git-recent-all-branches() {
-  local SELECTED_BRANCH="$(git for-each-ref --format='%(refname)' --sort=-committerdate | \
-                           sed -e 's|^refs/\(heads\|remotes\)/||'                       | \
-                           fzf)"
-  if [ -n "$SELECTED_BRANCH" ]; then
-    if [ -n "$BUFFER" ]; then
-      BUFFER="${BUFFER}${SELECTED_BRANCH}"
-    else
-      BUFFER="git checkout -t ${SELECTED_BRANCH}"
+  # git-add with peco
+  # ref: http://petitviolet.hatenablog.com/entry/20140722/1406034439
+  function peco-git-add() {
+    local SELECTED_FILE_TO_ADD="$(git status --porcelain  | \
+                                  fzf --query "$LBUFFER" | \
+                                  awk -F ' ' '{print $NF}')"
+    if [ -n "$SELECTED_FILE_TO_ADD" ]; then
+      BUFFER="git add $(echo "$SELECTED_FILE_TO_ADD" | tr '\n' ' ')"
+      CURSOR=$#BUFFER
       zle accept-line
     fi
-  fi
-  zle clear-screen
-}
-zle -N peco-git-recent-all-branches
-bindkey "^g^g^b"  peco-git-recent-all-branches
+  }
+  zle -N peco-git-add
+  bindkey "^ga"  peco-git-add
+  bindkey "^g^a" peco-git-add
+
+  function peco-git-recent-all-branches() {
+    local SELECTED_BRANCH="$(git for-each-ref --format='%(refname)' --sort=-committerdate | \
+                             sed -e 's|^refs/\(heads\|remotes\)/||'                       | \
+                             fzf)"
+    if [ -n "$SELECTED_BRANCH" ]; then
+      if [ -n "$BUFFER" ]; then
+        BUFFER="${BUFFER}${SELECTED_BRANCH}"
+      else
+        BUFFER="git checkout -t ${SELECTED_BRANCH}"
+        zle accept-line
+      fi
+    fi
+    zle clear-screen
+  }
+  zle -N peco-git-recent-all-branches
+  bindkey "^g^g^b"  peco-git-recent-all-branches
+fi
