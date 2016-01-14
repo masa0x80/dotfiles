25245f791c883250ac5de0ea7b9ff059d930b3f8
Kimura Masayuki <masa0x80@gmail.com>
2016-01-14 12:27:42 +0900

    Fix aliases for ruby

--- .zsh/alias/rails.zsh
+++ .zsh/alias/rails.zsh
@@ -2,6 +2,8 @@ alias s='spring rails s -p $RAILS_SERVER_PORT'
 alias ss="pkill -f 'ruby .*/bin/spring'; pkill -f 'spring app'; pkill -f 'spring server'"
 alias c='rails c'
 alias t='rspec'
-alias bi='bundle install --path=vendor/bundle'
-alias bil='bundle install --path=vendor/bundle --local'
+alias bi='bundle install'
+alias bil='bundle install --local'
 alias db='rails db'
+
+alias -g BO='--path=vendor/bundle --binstubs=vendor/bin --jobs=4'
