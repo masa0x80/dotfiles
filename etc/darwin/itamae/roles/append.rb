f4e8d5e92d88da7843f3b07deeb96c25e66f6536
Kimura Masayuki <masa0x80@gmail.com>
2016-02-14 18:41:09 +0900

    Add python3 install recipe for mac

--- etc/darwin/itamae/roles/append.rb
+++ etc/darwin/itamae/roles/append.rb
@@ -4,6 +4,9 @@ node[:recipes] = node[:recipes].concat(
       path: 'zplug/install.rb',
       tags: %w[append zplug],
     }, {
+      path: 'pyenv-neovim/install.rb',
+      tags: %w[append pyenv-neovim],
+    }, {
       path: 'vim-plug/install.rb',
       tags: %w[append vim vim-plug-only],
     }, {
