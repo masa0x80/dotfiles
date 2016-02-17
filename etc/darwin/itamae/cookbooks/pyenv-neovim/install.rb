58c4b0ac51e9a66003870645d550bfcd95bfa8a8
Kimura Masayuki <masa0x80@gmail.com>
2016-02-18 01:11:31 +0900

    Revert "Fix python install recipe"
    We need python2 to make vim.
    
    This reverts commit 1716d02cbb4d38ba4f52bda9abd96c15b6d40560.

--- etc/darwin/itamae/cookbooks/pyenv-neovim/install.rb
+++ etc/darwin/itamae/cookbooks/pyenv-neovim/install.rb
@@ -1,6 +1,11 @@
 include_recipe '../pyenv/install.rb'
 
+execute 'install python3' do
+  command "zsh -lc 'pyenv install #{node[:python][:version3]} && pyenv global #{node[:python][:version3]} #{node[:python][:version2]}'"
+  not_if  "type -a pyenv && pyenv versions | grep #{node[:python][:version3]}"
+end
+
 execute 'pip install neovim' do
-  command "zsh -lc 'pip install neovim'"
-  not_if  "zsh -lc 'pip list | grep neovim'"
+  command "zsh -lc 'pip3 install neovim'"
+  not_if  "zsh -lc 'pip3 list | grep neovim'"
 end
