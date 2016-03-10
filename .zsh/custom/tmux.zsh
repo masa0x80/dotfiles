176c30f4d093ef38d42d04cd4bddcd3e767580be
Kimura Masayuki <masa0x80@gmail.com>
2016-03-10 12:47:21 +0900

    Improve tmux auto attach

--- .zsh/custom/tmux.zsh
+++ .zsh/custom/tmux.zsh
@@ -1,13 +1,21 @@
+# ref: http://qiita.com/b4b4r07/items/01359e8a3066d1c37edc
 function is_tmux_running() { [ ! -z "$TMUX" ]; }
 function shell_has_started_interactively() { [ ! -z "$PS1" ]; }
-function is_ssh_running() { [ ! -z "$SSH_CONECTION" ]; }
+function is_ssh_running() { [ ! -z "$SSH_CONNECTION" ]; }
 
 function tmux_attach_session() {
   if ! is_tmux_running; then
     if shell_has_started_interactively && ! is_ssh_running; then
       if (( $+commands[tmux] )); then
         if tmux has-session >/dev/null 2>&1; then
-          tmux attach-session
+          tmux list-sessions
+          echo -n "Tmux: attach? (y/N/num) "
+          read
+          if [[ "$REPLY" =~ ^[Yy]$ ]] || [[ "$REPLY" == '' ]]; then
+            tmux attach-session
+          else
+            tmux attach -t "$REPLY"
+          fi
         else
           tmux
         fi
