c49c1602ac49b9a50384ba912913a1ace181a262
Kimura Masayuki <masa0x80@gmail.com>
2016-03-10 21:38:55 +0900

    Fix peco/fzf config

--- .zsh/custom/fzf.zsh
+++ .zsh/custom/fzf.zsh
@@ -1,20 +1,3 @@
 if (( $+commands[fzf] )); then
   bindkey '^Q' fzf-file-widget
-
-  function peco-git-recent-all-branches() {
-    local SELECTED_BRANCH="$(git for-each-ref --format='%(refname)' --sort=-committerdate | \
-                             sed -e 's|^refs/\(heads\|remotes\)/||'                       | \
-                             fzf)"
-    if [ -n "$SELECTED_BRANCH" ]; then
-      if [ -n "$BUFFER" ]; then
-        BUFFER="${BUFFER}${SELECTED_BRANCH}"
-      else
-        BUFFER="git checkout -t ${SELECTED_BRANCH}"
-        zle accept-line
-      fi
-    fi
-    zle clear-screen
-  }
-  zle -N peco-git-recent-all-branches
-  bindkey "^g^g^b"  peco-git-recent-all-branches
 fi
--- .zsh/custom/peco.zsh
+++ .zsh/custom/peco.zsh
@@ -33,6 +33,23 @@ if (( $+commands[peco] )); then
   bindkey "^ga"  peco-git-add
   bindkey "^g^a" peco-git-add
 
+  function peco-git-recent-all-branches() {
+    local SELECTED_BRANCH="$(git for-each-ref --format='%(refname)' --sort=-committerdate | \
+                             sed -e 's|^refs/\(heads\|remotes\)/||'                       | \
+                             peco)"
+    if [ -n "$SELECTED_BRANCH" ]; then
+      if [ -n "$BUFFER" ]; then
+        BUFFER="${BUFFER}${SELECTED_BRANCH}"
+      else
+        BUFFER="git checkout -t ${SELECTED_BRANCH}"
+        zle accept-line
+      fi
+    fi
+    zle clear-screen
+  }
+  zle -N peco-git-recent-all-branches
+  bindkey "^g^g^b"  peco-git-recent-all-branches
+
   function peco-gst(){
     git status -s -uno | peco --query "$*" | cut -d ' ' -f 3
   }
