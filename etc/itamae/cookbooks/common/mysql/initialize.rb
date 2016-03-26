87bb3efd81f352389e91a0ba8a59eff58a5ec882
Kimura Masayuki <masa0x80@gmail.com>
2016-03-27 02:24:08 +0900

    Fix mysql initialize recipe

--- etc/itamae/cookbooks/common/mysql/initialize.rb
+++ etc/itamae/cookbooks/common/mysql/initialize.rb
@@ -1,11 +1,11 @@
-execute 'mysql.server start' do
-  only_if 'uname | grep Darwin'
+execute 'brew services start mysql' do
+  only_if 'uname | grep Darwin && mysql.server status | grep "is not running"'
 end
 
 execute 'initialize mysql' do
   command <<-'EOF'
     TEMP_PASSWORD='Passw0rd!'
-    mysql -uroot -p"$(grep 'password is generated' /var/log/mysqld.log | cut -d' ' -f 11)" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${TEMP_PASSWORD}';" --connect-expired-password
+    mysql -uroot -p"$(grep 'password is generated' /var/log/mysqld.log | cut -d' ' -f 11 | sed -e 's/"/\\"/g')" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${TEMP_PASSWORD}';" --connect-expired-password
     mysql -uroot -p${TEMP_PASSWORD} -e "UNINSTALL PLUGIN validate_password;"
     mysql -uroot -p${TEMP_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '' PASSWORD EXPIRE NEVER;"
   EOF
