22a485d65bdef77ec7793300b1a1d9d2115858f5
Kimura Masayuki <masa0x80@gmail.com>
2016-04-01 00:54:27 +0900

    Remove fzf keybindings

--- .zsh/custom/fzf.zsh
+++ .zsh/custom/fzf.zsh
@@ -1,15 +1,7 @@
 if (( $+commands[fzf] )); then
-  if [[ ! "$PATH" == *$HOME/.zplug/repos/junegunn/fzf/bin* ]]; then
-    export PATH="$PATH:$HOME/.zplug/repos/junegunn/fzf/bin"
-  fi
-
   if [[ ! "$MANPATH" == *$HOME/.zplug/repos/junegunn/fzf/man* && -d "$HOME/.zplug/repos/junegunn/fzf/man" ]]; then
     export MANPATH="$MANPATH:$HOME/.zplug/repos/junegunn/fzf/man"
   fi
 
   [[ $- == *i* ]] && source "$HOME/.zplug/repos/junegunn/fzf/shell/completion.zsh" 2> /dev/null
-
-  source "$HOME/.zplug/repos/junegunn/fzf/shell/key-bindings.zsh"
-
-  bindkey '^Q' fzf-file-widget
 fi
