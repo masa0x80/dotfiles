c4611f558c2e6dd0243cfd48e9b8cc75c14f947a
Kimura Masayuki <masa0x80@gmail.com>
2016-04-01 01:29:48 +0900

    Fix mysql init script

--- etc/itamae/cookbooks/common/mysql/initialize.rb
+++ etc/itamae/cookbooks/common/mysql/initialize.rb
@@ -5,10 +5,9 @@ end
 execute 'initialize mysql' do
   command <<-'EOF'
     TEMP_PASSWORD='Passw0rd!'
-    mysql -uroot -p"$(grep 'password is generated' /var/log/mysqld.log | cut -d' ' -f 11 | sed -e 's/"/\\"/g')" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${TEMP_PASSWORD}';" --connect-expired-password
+    mysql -uroot -p"$(sudo grep 'password is generated' /var/log/mysqld.log | cut -d' ' -f 11 | sed -e 's/"/\\"/g')" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${TEMP_PASSWORD}';" --connect-expired-password
     mysql -uroot -p${TEMP_PASSWORD} -e "UNINSTALL PLUGIN validate_password;"
     mysql -uroot -p${TEMP_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '' PASSWORD EXPIRE NEVER;"
   EOF
-  user     'root'
-  only_if  "mysql -uroot -e 'exit'; [ $? == 1 ] && grep 'password is generated' /var/log/mysqld.log"
+  only_if  "mysql -uroot -e 'exit'; [ $? == 1 ] && sudo grep 'password is generated' /var/log/mysqld.log"
 end
