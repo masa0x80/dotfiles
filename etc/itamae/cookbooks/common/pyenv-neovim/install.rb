a66b3b098570c01f099259b73ae62ecfd429876b
Kimura Masayuki <masa0x80@gmail.com>
2016-04-03 01:31:43 +0900

    Fix python version

--- etc/itamae/cookbooks/common/pyenv-neovim/install.rb
+++ etc/itamae/cookbooks/common/pyenv-neovim/install.rb
@@ -1,7 +1,7 @@
 include_recipe '../pyenv/install.rb'
 
 execute 'install python3' do
-  command "zsh -lc 'pyenv install #{node[:python][:version3]} && pyenv global #{node[:python][:version3]} #{node[:python][:version2]}'"
+  command "zsh -lc 'pyenv install #{node[:python][:version3]} && pyenv global #{node[:python][:version2]} #{node[:python][:version3]}'"
   not_if  "type -a pyenv && pyenv versions | grep #{node[:python][:version3]}"
 end
 
