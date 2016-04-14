3173449e05b742d7f9ea8a623696a840745ce6f8
Kimura Masayuki <masa0x80@gmail.com>
2016-04-15 00:09:25 +0900

    Fix ImageMagick install recipe

--- etc/itamae/cookbooks/linux/imagemagick/install.rb
+++ etc/itamae/cookbooks/linux/imagemagick/install.rb
@@ -1,11 +1,13 @@
-%w[
-  ImageMagick-last
-  ImageMagick-last-devel
-  ImageMagick-last-libs
-].each do |name|
-  package name do
-    action :install
-    options '--enablerepo=remi'
-    user   'root'
+%i[remove install].each do |type|
+  %w[
+    ImageMagick-last
+    ImageMagick-last-devel
+    ImageMagick-last-libs
+  ].each do |name|
+    package name do
+      action  type
+      options '--enablerepo=remi'
+      user   'root'
+    end
   end
 end
