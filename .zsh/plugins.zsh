96e45dc0875dba96f6e130be730fa959da503b14
Kimura Masayuki <masa0x80@gmail.com>
2016-05-01 23:46:29 +0900

    Upgrade zplug & fix zplug revision

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,5 +1,7 @@
 if [ ! -e $HOME/.zplug/zplug ]; then
+  ZPLUG_REVISION='a9a82619549fa03fb1d315490169f89e9d7898a6'
   git clone https://github.com/b4b4r07/zplug $HOME/.zplug --depth=1
+  cd $HOME/.zplug/ && git checkout $ZPLUG_REVISION
 fi
 
 if [ -r $HOME/.zplug/zplug ]; then
@@ -9,7 +11,7 @@ if [ -r $HOME/.zplug/zplug ]; then
   zplug 'sindresorhus/pure'
 
   zplug 'mollifier/anyframe'
-  zplug 'b4b4r07/enhancd', of:enhancd.sh
+  zplug 'b4b4r07/enhancd', use:enhancd.sh
 
   zplug 'zsh-users/zsh-autosuggestions'
   zplug 'zsh-users/zsh-completions'
@@ -20,17 +22,17 @@ if [ -r $HOME/.zplug/zplug ]; then
       from:gh-r
     zplug 'jpmens/jo', \
       as:command, \
-      do:'cd $HOME/.zplug/repos/jpmens/jo && autoreconf -i && ./configure --prefix=$HOME/.zplug && make check && make install'
+      hook-build:'cd $HOME/.zplug/repos/jpmens/jo && autoreconf -i && ./configure --prefix=$HOME/.zplug && make check && make install'
 
     zplug 'junegunn/fzf-bin', \
       as:command, \
       from:gh-r, \
-      file:fzf, \
-      of:"*linux*amd64*", \
+      rename-to:fzf, \
+      use:"*linux*amd64*", \
       on:junegunn/fzf
     zplug 'junegunn/fzf', \
       as:command, \
-      of:bin/fzf-tmux
+      use:bin/fzf-tmux
     zplug 'peco/peco', \
       as:command, \
       from:gh-r
@@ -42,12 +44,12 @@ if [ -r $HOME/.zplug/zplug ]; then
 
     zplug 'monochromegane/the_platinum_searcher', \
       from:gh-r, \
-      do:"mkdir -p $HOME/.zplug/bin; ln -s $HOME/.zplug/{repos/**/*/pt,bin/pt}"
+      hook-build:"mkdir -p $HOME/.zplug/bin; ln -s $HOME/.zplug/{repos/**/*/pt,bin/pt}"
 
     zplug 'monwhitaker/gibo', \
-      file:gibo, \
+      rename-to:gibo, \
       as:command, \
-      do:'$HOME/.zplug/repos/simonwhitaker/gibo/gibo -u'
+      hook-build:'$HOME/.zplug/repos/simonwhitaker/gibo/gibo -u'
   fi
 
   if ! zplug check --verbose; then
