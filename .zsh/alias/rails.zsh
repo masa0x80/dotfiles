2eed6e3436e0e949232f554704eaf8eea6dc3d1c
Kimura Masayuki <masa0x80@gmail.com>
2016-05-11 23:46:56 +0900

    [zsh] bundle with gtags execution

--- .zsh/alias/rails.zsh
+++ .zsh/alias/rails.zsh
@@ -3,6 +3,18 @@ s() {
   tmux rename-window ${window_name}/server
   command rails s -p $RAILS_SERVER_PORT
 }
+
+bundle() {
+  command bundle $@
+  if (( $+commands[gtags] )); then
+    local log_file=/tmp/gtags-$$
+    echo ''
+    echo "${fg[magenta]}gtags error log: $log_file${fg[default]}"
+    echo ''
+    (gtags 2> $log_file) &
+  fi
+}
+
 alias ss="pkill -f 'ruby .*/bin/spring'; pkill -f 'spring app'; pkill -f 'spring server'"
 alias c='rails c'
 alias t='rspec'
