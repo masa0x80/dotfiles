88e0fa13c7cdd597ee889c3c8e2be5aaeacef044
Kimura Masayuki <masa0x80@gmail.com>
2016-06-15 12:46:06 +0900

    Fix HOMEBREW_CASK_OPTS

--- .zsh/os/darwin/rc/zshrc.zsh
+++ .zsh/os/darwin/rc/zshrc.zsh
@@ -1,4 +1,4 @@
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/opt/homebrew-cask/Caskroom"
 
 # ref: https://github.com/yyuu/pyenv/wiki/Common-build-problems#build-failed-error-the-python-zlib-extension-was-not-compiled-missing-the-zlib
 export CFLAGS="-I$(xcrun --show-sdk-path)/usr/include"
