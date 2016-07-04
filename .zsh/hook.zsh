6e1277df82edba05b8fc503ca493b3fbc35c6a1f
Kimura Masayuki <masa0x80@gmail.com>
2016-07-04 23:23:18 +0900

    [zsh] Add private git config presence check

--- .zsh/hook.zsh
+++ .zsh/hook.zsh
@@ -2,7 +2,14 @@ is_tmux_running() {
   [ ! -z "$TMUX" ]
 }
 
+check_private_git_config() {
+  if [ ! -f $HOME/.private/git/config ]; then
+    echo -e '\033[7;36m[WARN] Private git config file is not found.\033[0;39m'
+  fi
+}
+
 rename_window() {
+  check_private_git_config
   if is_tmux_running; then
     if [ -e .git ]; then
       tmux rename-window "$PWD:h:t/$PWD:t"
