f956bb3f722cd40ecf7d8cd5597687fe98700f98
Kimura Masayuki <masa0x80@gmail.com>
2016-08-11 13:21:28 +0900

    :smile: Install emojify on CentOS

--- etc/init/centos/install.sh
+++ etc/init/centos/install.sh
@@ -8,24 +8,27 @@ if [ -z "$(sudo sh -c 'grep "export PATH=" /root/.zshrc 2> /dev/null')" ]; then
   sudo sh -c "echo 'export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin' >> /root/.zshrc"
 fi
 
-# Proxy設定
+# Proxy config
 if [ ! -z $HTTP_PROXY ]; then
   if [ -z `grep "proxy=$HTTP_PROXY" /etc/yum.conf` ]; then
     sudo sh -c "echo 'proxy=$HTTP_PROXY' >> /etc/yum.conf"
   fi
 fi
 
-# 日本のサーバーを使うように
+# Use jp servers
 if [ -z `grep '^include_only=.jp$' /etc/yum/pluginconf.d/fastestmirror.conf` ]; then
   sudo sh -c "echo 'include_only=.jp' >> /etc/yum/pluginconf.d/fastestmirror.conf"
 fi
 
-# gitのインストール
+# Install git
 if type -a git > /dev/null 2>&1; then
   echo 'skip git install'
 else
   sudo -E yum install -y git which bzip2 unzip curl
 fi
 
-# rubyのインストールの準備
+# Install emojify ref: https://github.com/mrowa44/emojify
+sudo sh -c "curl https://raw.githubusercontent.com/mrowa44/emojify/master/emojify -o /usr/local/bin/emojify && chmod +x /usr/local/bin/emojify"
+
+# Prepare to install ruby
 sudo -E yum install -y patch readline-devel zlib-devel openssl-devel gcc-c++ bzip2-devel sqlite-devel libffi-devel libxslt-devel autoconf
