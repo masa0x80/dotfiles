7952b922c3286af91a9242f95b18df2e65dbaff5
Kimura Masayuki <masa0x80@gmail.com>
2016-08-11 11:43:24 +0900

    [itamae] Fix vim install recipe

--- etc/init/itamae/cookbooks/linux/vim/install.rb
+++ etc/init/itamae/cookbooks/linux/vim/install.rb
@@ -15,17 +15,17 @@ end
 
 execute 'download vim' do
   command "cd #{node[:src_dir]} && wget ftp://ftp.vim.org/pub/vim/unix/vim-#{node[:vim][:version]}.tar.bz2"
-  not_if "#{node[:src_dir]}/vim-#{node[:vim][:version]}.tar.bz2"
+  not_if "ls #{node[:src_dir]}/vim-#{node[:vim][:version]}.tar.bz2"
 end
 
 execute 'extract vim' do
   command "cd #{node[:src_dir]} && tar jxf vim-#{node[:vim][:version]}.tar.bz2"
+  only_if "ls #{node[:src_dir]}/vim-#{node[:vim][:version]}.tar.bz2"
 end
-
-include_recipe '../../common/pyenv/install.rb'
 
 local_ruby_block 'install vim' do
   block do
     `cd #{node[:src_dir]}/vim#{node[:vim][:version].to_s.gsub(/\./, '')} && ./configure #{node[:vim][:make_options]} && make && sudo make install`
   end
+  not_if "type -a vim && command vim --version | grep #{node[:vim][:version]}"
 end
