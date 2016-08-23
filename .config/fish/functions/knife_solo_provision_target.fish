66e9c89162bf945540ac4450853f18bee458cc11
Kimura Masayuki <masa0x80@gmail.com>
2016-08-23 12:57:03 +0900

    [fish] Fix knife_solo_provision_* functions

--- .config/fish/functions/knife_solo_provision_target.fish
+++ .config/fish/functions/knife_solo_provision_target.fish
@@ -1,3 +1,7 @@
 function knife_solo_provision_target
-  select-target-host | read -l target_host; vagrant destroy -f && vagrant up && krepare $target_host && kook $target_host
+  select_target_host | read -l target_host; \
+        and vagrant destroy -f; \
+        and vagrant up; \
+        and krepare $target_host; \
+        and kook $target_host
 end
--- .config/fish/functions/knife_solo_provision_vagrant.fish
+++ .config/fish/functions/knife_solo_provision_vagrant.fish
@@ -1,3 +1,10 @@
 function knife_solo_provision_vagrant
-  select-vagrant-host | read -l vagrant_host; vagrant destroy -f && vagrant up && krepare $vagrant_host && kook $vagrant_host
+  select_vagrant_host | read -l vagrant_host; \
+        and vagrant destroy -f; \
+        and vagrant up; \
+        and vagrant snapshot save init; \
+        and krepare $vagrant_host; \
+        and vagrant snapshot save prepared; \
+        and kook $vagrant_host; \
+        and vagrant snapshot save cooked
 end
