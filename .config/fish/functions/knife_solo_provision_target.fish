3bb35b7f96d211f8d35629fdc7773f9bcd28b78c
Kimura Masayuki <masa0x80@gmail.com>
2016-08-25 00:46:42 +0900

    [fish] Fix bugs

--- .config/fish/functions/knife_solo_provision_target.fish
+++ .config/fish/functions/knife_solo_provision_target.fish
@@ -1,7 +1,5 @@
 function knife_solo_provision_target
   select_target_host | read -l target_host; \
-        and vagrant destroy -f; \
-        and vagrant up; \
         and krepare $target_host; \
         and kook $target_host
 end
--- .config/fish/functions/select_ghq_cd.fish
+++ .config/fish/functions/select_ghq_cd.fish
@@ -3,9 +3,11 @@ function select_ghq_cd
   ghq list --full-path | \
         sed -e "s|$HOME/||g" | \
         fzf --query "$buffer" | \
+        sed -e 's/ /\\\\ /g' | \
         read -l repository_path
   if test -n "$repository_path"
-    cd ~/(echo $repository_path | sed -e 's/ /\\ /g')
+    commandline "cd ~/$repository_path"
+    commandline -f execute
   end
   commandline -f repaint
 end
