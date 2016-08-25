05e5a68de74d79b6faf46d2246c34d4fb20bc219
Kimura Masayuki <masa0x80@gmail.com>
2016-08-25 12:23:37 +0900

    [fish] Fix key bind

--- .config/fish/functions/scrapbook.fish
+++ .config/fish/functions/scrapbook.fish
@@ -3,7 +3,7 @@ function scrapbook
     set -q $SCRAPBOOK_DIR; and set -x SCRAPBOOK_DIR $HOME/.scrapbook
     find $SCRAPBOOK_DIR -type f | fzf --query "$argv" | read -l selected_line
     if not set -q $selected_line
-      mdv $selected_line
+      commandline "mdv $selected_line"
     end
     commandline -f repaint
   end
