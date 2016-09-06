26c4cead43ea9e8bcb2a17646cce6c9ab463a549
Kimura Masayuki <masa0x80@gmail.com>
2016-09-07 01:23:46 +0900

    [fish] Add --no-sort option to __select_history

--- .config/fish/functions/__select_history.fish
+++ .config/fish/functions/__select_history.fish
@@ -1,6 +1,6 @@
 function __select_history
   commandline | read -l buffer
-  history | fzf --query "$buffer" | read -l command
+  history | fzf --no-sort --query "$buffer" | read -l command
   if test -n "$command"
     commandline $command
   end
