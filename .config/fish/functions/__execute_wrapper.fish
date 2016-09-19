929af4d9f31e7d0f0b9f4b57aed5c36bffbe2dfe
Kimura Masayuki <masa0x80@gmail.com>
2016-09-19 13:12:06 +0900

    [fish] Add useful keywords to __execute_wrapper

--- .config/fish/functions/__execute_wrapper.fish
+++ .config/fish/functions/__execute_wrapper.fish
@@ -1,22 +1,40 @@
 function __execute_wrapper
   commandline | read -l buffer
 
-  string replace -a ';' ' ;' $buffer | read buffer
+  string replace -a -r '(;|\|)' ' $1' $buffer | read buffer
 
   for word in (string split ' ' $buffer)
-    for keyword in GST TH VH GH
+    for keyword in F G H L N N1 N2 P T GST TH VH GH
       if test "$word" = "$keyword"
         switch $word
+          case F
+            set replacement '| fzf'
+          case G
+            set replacement '| egrep --color=auto'
+          case H
+            set replacement '| head'
+          case L
+            set replacement '| less'
+          case N
+            set replacement ' >/dev/null ^/dev/null'
+          case N1
+            set replacement ' >/dev/null'
+          case N2
+            set replacement ' ^/dev/null'
+          case P
+            set replacement '| peco'
+          case T
+            set replacement '| tail'
           case GST
-            __select_git_status | string trim | read selected
+            __select_git_status | string trim | read replacement
           case TH
-            __select_target_host | read selected
+            __select_target_host | read replacement
           case VH
-            __select_vagrant_host | read selected
+            __select_vagrant_host | read replacement
           case GH
-            git fzf | read selected
+            git fzf | read replacement
         end
-        string replace $keyword $selected $buffer | read buffer
+        string replace $keyword $replacement $buffer | read buffer
       end
     end
   end
@@ -31,7 +49,7 @@ function __execute_wrapper
   end
 
   string replace -a -r '\s+' ' ' $buffer | read buffer
-  string replace -a ' ;' ';'     $buffer | read buffer
+  string replace -a ' ;' ';' $buffer | read buffer
 
   commandline $buffer
   commandline -f execute
