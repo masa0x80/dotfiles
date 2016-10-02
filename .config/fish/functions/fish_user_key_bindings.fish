0ba9fe078940a8d6ce91adfad91e769893b1af0e
Kimura Masayuki <masa0x80@gmail.com>
2016-10-03 00:04:34 +0900

    [fish] Extract functions

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -6,10 +6,6 @@ function fish_user_key_bindings
   bind \cgc __select_ghq_cd
   bind \cr __select_history
 
-  bind \cg\cg\cb __git_recent_all_branches
-  bind \cgb   __git_recent_branches
-  bind \cg\cb __git_recent_branches
-
   bind \cj __execute_wrapper
   bind \r  __execute_wrapper
 end
--- .config/fish/plugins.fish
+++ .config/fish/plugins.fish
@@ -1,4 +1,5 @@
-oh-my-fish/plugin-cd
 oh-my-fish/plugin-osx
+masa0x80/plugin-cd
 masa0x80/complement_ssh_host.fish
 masa0x80/complement_mkr_host_ip.fish
+masa0x80/git_checkout_keybind.fish
