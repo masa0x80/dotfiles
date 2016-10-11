df625d1f9db54a0e901a65ddff3e0348540cadb1
Kimura Masayuki <masa0x80@gmail.com>
2016-10-12 00:20:44 +0900

    [fish] Extract git-add keybinding

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,6 @@
 function fish_user_key_bindings
   bind \cs\cb scrapbook
 
-  bind \cg\ca __select_git_add
-  bind \cga   __select_git_add
-
   bind \cj __execute_wrapper
   bind \r  __execute_wrapper
 end
--- .config/fish/plugins.fish
+++ .config/fish/plugins.fish
@@ -3,6 +3,7 @@ oh-my-fish/plugin-cd
 masa0x80/angler.fish
 masa0x80/complement_ssh_host.fish
 masa0x80/complement_mkr_host_ip.fish
+masa0x80/git_add_keybind.fish
 masa0x80/git_checkout_keybind.fish
 masa0x80/ghq_cd_keybind.fish
 masa0x80/filtering_history_keybind.fish
