ae08ffb0648df5bef23ded3891a7bfce968e03b0
Kimura Masayuki <masa0x80@gmail.com>
2016-12-11 22:57:00 +0900

    [fish] Rename pulgins

--- .config/fish/plugins.fish
+++ .config/fish/plugins.fish
@@ -1,8 +1,8 @@
 oh-my-fish/plugin-osx
 oh-my-fish/plugin-cd
 masa0x80/angler.fish
-masa0x80/complement_ssh_host.fish
-masa0x80/complement_mkr_host_ip.fish
+masa0x80/complete_ssh_host.fish
+masa0x80/complete_mkr_host_ip.fish
 masa0x80/git_add_keybind.fish
 masa0x80/git_checkout_keybind.fish
 masa0x80/ghq_cd_keybind.fish
