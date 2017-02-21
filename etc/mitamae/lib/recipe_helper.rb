6123a51bf93a2c3515eaa09e59ae0b7fcb6aa987
Kimura Masayuki <masa0x80@gmail.com>
2017-02-22 00:48:07 +0900

    [mitamae] Add `git_clone` helper method

--- etc/mitamae/lib/recipe_helper.rb
+++ etc/mitamae/lib/recipe_helper.rb
@@ -1,29 +1,41 @@
-MItamae::RecipeContext.class_eval do
-  def include_cookbook(name)
-    root_dir = File.expand_path('../..', __FILE__)
-    include_recipe File.join(root_dir, 'cookbooks', name, 'default')
+define :include_cookbook do
+  root_dir = File.expand_path('../..', __FILE__)
+  include_recipe File.join(root_dir, 'cookbooks', params[:name], 'default')
+end
+
+define :include_role do
+  root_dir = File.expand_path('../..', __FILE__)
+  include_recipe File.join(root_dir, 'roles', params[:name], 'default')
+end
+
+define :cask do
+  pkg = params[:name]
+
+  # install app
+  execute "brew cask install #{pkg}" do
+    only_if "brew cask info #{pkg} | grep -q 'Not installed'"
   end
 
-  def include_role(name)
-    root_dir = File.expand_path('../..', __FILE__)
-    include_recipe File.join(root_dir, 'roles', name, 'default')
+  # remove old dir
+  caskroom_dir = '/usr/local/Caskroom'
+  app_dir = File.join(caskroom_dir, pkg)
+  current = `brew cask info #{pkg} | grep '#{app_dir}' | cut -d ' ' -f 1`.chomp
+  Dir.glob(File.join(app_dir, '*')).each do |dir|
+    version = File.basename(dir)
+    execute "rm -rf #{File.join(app_dir, version)}" do
+      only_if "test '#{current} != '#{dir}'"
+    end
   end
+end
 
-  def cask(pkg)
-    # install app
-    execute "brew cask install #{pkg}" do
-      only_if "brew cask info #{pkg} | grep -q 'Not installed'"
-    end
-
-    # remove old dir
-    caskroom_dir = '/usr/local/Caskroom'
-    app_dir = File.join(caskroom_dir, pkg)
-    current = `brew cask info #{pkg} | grep '#{app_dir}' | cut -d ' ' -f 1`.chomp
-    Dir.glob(File.join(app_dir, '*')).each do |dir|
-      version = File.basename(dir)
-      execute "rm -rf #{File.join(app_dir, version)}" do
-        only_if "test '#{current} != '#{dir}'"
-      end
-    end
+define :git_clone, repository: nil, user: nil do
+  dest_path = params[:name]
+  execute "git_clone[#{dest_path}]" do
+    command <<-"EOF"
+      #{node[:proxy_config]}
+      git clone #{params[:repository]} #{dest_path}
+    EOF
+    user user || node[:user]
+    not_if "test -d #{dest_path}"
   end
 end
