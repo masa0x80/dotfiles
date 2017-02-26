27687128a06d8b0b60887185765b995e18d539ec
Kimura Masayuki <masa0x80@gmail.com>
2017-02-27 00:07:16 +0900

    [mitamae] Small fix

--- etc/mitamae/init.sh
+++ etc/mitamae/init.sh
@@ -5,11 +5,8 @@ sh ${dir_name}/bin/setup
 
 case $(uname) in
   Darwin)
-    if type -a xcode-select > /dev/null 2>&1; then
-      echo ' SKIP : xcode-select installation'
-    else
-      xcode-select --install
-    fi
+    brew update
+    brew upgrade
     ${dir_name}/bin/mitamae local bootstrap.rb
     ;;
   *)
--- etc/mitamae/roles/os/darwin/default.rb
+++ etc/mitamae/roles/os/darwin/default.rb
@@ -1,3 +1,7 @@
+execute 'xcode --install' do
+  not_if 'type -a xcode-select > /dev/null 2>&1'
+end
+
 include_role 'common'
 include_role 'append'
 
