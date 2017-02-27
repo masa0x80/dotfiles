f4678378cb4194bfd23ffdc2f45ceeade181e25e
Kimura Masayuki <masa0x80@gmail.com>
2017-02-28 00:56:09 +0900

    [mitamae] Install awscli

--- etc/mitamae/cookbooks/aws/default.rb
+++ etc/mitamae/cookbooks/aws/default.rb
@@ -1,1 +1,13 @@
 package 's3cmd'
+
+include_cookbook 'pyenv'
+
+execute 'pip install awscli' do
+  command <<-"EOF"
+    #{node[:proxy_config]}
+    export PATH=#{node[:home]}/.anyenv/bin:$PATH
+    eval "$(anyenv init -)"
+    pip list | grep awscli || pip install awscli
+  EOF
+  user node[:user]
+end
