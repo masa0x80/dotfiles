396c2ac6e1dc5586a0f7a05e64c7b64d9223a612
Kimura Masayuki <masa0x80@gmail.com>
2017-05-08 01:21:54 +0900

    [fish] Tweaks

--- .config/fish/functions/scrapbook.fish
+++ .config/fish/functions/scrapbook.fish
@@ -1,10 +1,9 @@
 function scrapbook
   if type -qa fzf; and type -qa mdv; and test -e $SCRAPBOOK_DIR
-    set -q $SCRAPBOOK_DIR; and set -x SCRAPBOOK_DIR $HOME/.scrapbook
+    set -q SCRAPBOOK_DIR; and set -x SCRAPBOOK_DIR $HOME/.scrapbook
     find $SCRAPBOOK_DIR -type f | fzf --query "$argv" | read -l selected_line
-    if not set -q $selected_line
+    if set -q selected_line
       commandline "mdv $selected_line"
     end
-    commandline -f repaint
   end
 end
