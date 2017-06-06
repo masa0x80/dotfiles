f01bbd29bec717baff91b73dd2c3cf095376a5c8
Kimura Masayuki <masa0x80@gmail.com>
2017-06-06 23:13:40 +0900

    [mitamae] Fix imagemagic cookbook

--- etc/mitamae/cookbooks/imagemagick/default.rb
+++ etc/mitamae/cookbooks/imagemagick/default.rb
@@ -2,16 +2,4 @@ case node[:platform]
 when 'darwin'
   package 'imagemagick'
 when 'redhat'
-  %i(remove install).each do |type|
-    %w(
-      ImageMagick7
-      ImageMagick7-devel
-      ImageMagick7-libs
-    ).each do |pkg|
-      package pkg do
-        action  type
-        options '--enablerepo=remi'
-      end
-    end
-  end
 end
