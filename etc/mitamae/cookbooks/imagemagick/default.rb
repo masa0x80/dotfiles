23107553f373ff1c634b26eb05ae834dea3a015f
Kimura Masayuki <masa0x80@gmail.com>
2017-06-18 00:46:38 +0900

    [mitamae] Install imagemagick on linux

--- etc/mitamae/cookbooks/imagemagick/default.rb
+++ etc/mitamae/cookbooks/imagemagick/default.rb
@@ -2,4 +2,14 @@ case node[:platform]
 when 'darwin'
   # Install imagemagick via brew bundle
 when 'redhat'
+  %w(
+    ImageMagick7
+    ImageMagick7-devel
+    ImageMagick7-libs
+  ).each do |pkg|
+    package pkg do
+      action  :install
+      options '--enablerepo=remi'
+    end
+  end
 end
