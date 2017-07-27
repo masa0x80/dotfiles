3f534b9282daf937f52f88004f86d83942bde62f
KIMURA Masayuki <masa0x80@gmail.com>
2017-07-28 00:47:20 +0900

    [fish] Remove __execute_wrapper function

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -7,6 +7,6 @@ function fish_user_key_bindings
     bind ' ' '__gabbr_expand; commandline -i " "'
     bind ';' '__gabbr_expand; commandline -i "; "'
     bind \cj '__gabbr_expand; commandline -f execute'
-    bind \cm '__gabbr_expand; __execute_wrapper'
-    bind \r '__gabbr_expand; __execute_wrapper'
+    bind \cm '__gabbr_expand; commandline -f execute'
+    bind \r '__gabbr_expand; commandline -f execute'
 end
