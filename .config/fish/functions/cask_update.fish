7d7d158bc9605f7d35d86ab2f4af3e0f7830fa58
KIMURA Masayuki <masa0x80@gmail.com>
2017-07-28 00:47:02 +0900

    [fish] Update cask_update function

--- .config/fish/functions/cask_update.fish
+++ .config/fish/functions/cask_update.fish
@@ -1,7 +1,8 @@
 function cask_update
+    set -l packages (brew cask list | fzf --query "$argv")
     set -l caskroom_dir /usr/local/Caskroom
 
-    for pkg in $argv
+    for pkg in $packages
         echo $caskroom_dir/$pkg | read -l app_dir
         set -l latest_version (brew cask info $pkg | grep "$pkg:" | cut -d ' ' -f 2)
         if not test -d $app_dir/$latest_version
