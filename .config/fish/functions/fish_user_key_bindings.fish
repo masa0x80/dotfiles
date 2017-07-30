5a6f9f14d119907038913d04bd826cac1abd5671
KIMURA Masayuki <masa0x80@gmail.com>
2017-07-31 00:17:53 +0900

    [fish] Tweaks

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -5,7 +5,7 @@ function fish_user_key_bindings
     bind \cs\cb scrapbook
 
     bind ' ' '__gabbr_expand; commandline -i " "'
-    bind ';' '__gabbr_expand; commandline -i "; "'
+    bind ';' '__gabbr_expand; commandline -i ";"'
     bind \cj '__gabbr_expand; commandline -f execute'
     bind \cm '__gabbr_expand; commandline -f execute'
     bind \r '__gabbr_expand; commandline -f execute'
