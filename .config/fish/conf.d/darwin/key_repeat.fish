722326bfca07fa9daeb54d0b60ed684b451dbd64
KIMURA Masayuki <masa0x80@gmail.com>
2017-11-13 22:13:38 +0900

    [fish] Add function to accelerate key repeat

---
+++ .config/fish/conf.d/darwin/key_repeat.fish
@@ -0,0 +1,16 @@
+function speed_key_repeat
+    # delay until key repeat
+    defaults write NSGlobalDomain InitialKeyRepeat -int 12
+    # key repeat speed
+    defaults write NSGlobalDomain KeyRepeat -int 1
+    set_color -o yellow
+    echo
+    echo You must do logout your mac for taking this effect.
+    echo
+    set_color normal
+end
+
+function reset_key_repeat
+    defaults delete NSGlobalDomain KeyRepeat
+    defaults delete NSGlobalDomain InitialKeyRepeat
+end
