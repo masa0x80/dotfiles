750c8b085843b4edce6612cee4e51cbb5287263e
KIMURA Masayuki <masa0x80@gmail.com>
2018-01-16 01:27:00 +0900

    Fix fresco config

--- .config/fish/conf.d/fresco.fish
+++ .config/fish/conf.d/fresco.fish
@@ -1,7 +1,7 @@
 if type -qa ghq
-    set -l fresco_file (ghq root)/github.com/masa0x80/fresco/fresco.fish
-    if test -r $fresco_file
-        source $fresco_file
+    set -l bootstrap_file (ghq root)/github.com/masa0x80/fresco/fresco.fish
+    if test -r $bootstrap_file
+        source $bootstrap_file
     else
         ghq get masa0x80/fresco
     end
