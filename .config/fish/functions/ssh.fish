018a0c5e77ba3fee70fddf37e9affeba6e293714
KIMURA Masayuki <masa0x80@gmail.com>
2018-01-31 23:53:54 +0900

    Use `sshrc` if exists

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -1,5 +1,9 @@
 function ssh
     set -l window_name (tmux display -p '#{window_name}')
-    command ssh $argv
+    if type -qa sshrc
+        sshrc $argv
+    else
+        command ssh $argv
+    end
     tmux rename-window $window_name
 end
