bd8f93ca4b9e20afe225692f1a67c5ff87f230ed
KIMURA Masayuki <masa0x80@gmail.com>
2019-01-09 02:00:04 +0900

    [mitamae] :wrench: Update versions

--- etc/mitamae/attributes/versions.rb
+++ etc/mitamae/attributes/versions.rb
@@ -1,22 +1,22 @@
 node.reverse_merge!(
   git: {
-    version: '2.15.0'
+    version: '2.20.1'
   },
   global: {
-    version: '6.5.7'
+    version: '6.6.3'
   },
   golang: {
-    version: '1.9'
+    version: '1.11.4'
   },
   jo: {
-    version: '1.0'
+    version: '1.1'
   },
   mysql: {
     rpm_url: 'http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm',
     package: 'mysql57-community-release'
   },
   nodejs: {
-    version: 'v6.5.0'
+    version: '11.6.0'
   },
   perl: {
     version: '5.22.1'
@@ -25,14 +25,14 @@ node.reverse_merge!(
     version: '2.1.1'
   },
   ruby: {
-    version: '2.5.0'
+    version: '2.6.0'
   },
   remi_repo: {
     rpm_url: 'http://rpms.famillecollet.com/enterprise/remi-release-7.rpm',
     package: 'remi-release'
   },
   tmux: {
-    version: '2.4'
+    version: '2.8'
   },
   vagrant: {
     rpm_url: 'https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.rpm',
@@ -43,7 +43,7 @@ node.reverse_merge!(
     package: 'VirtualBox-5.1'
   },
   vim: {
-    version: '8.0',
+    version: '8.1',
     make_options: %w[
       --enable-fail-if-missing
       --enable-fontset
