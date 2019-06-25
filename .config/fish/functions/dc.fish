76a7adee3b4173da55944c9d277c7dc65ecc99ca
KIMURA Masayuki <masa0x80@gmail.com>
2019-06-26 00:48:19 +0900

    Improve dc command

--- .config/fish/functions/dc.fish
+++ .config/fish/functions/dc.fish
@@ -2,8 +2,9 @@ function dc
     set -l project_name (current_dir)
     set -l services ''
     if test "$argv[1]" = 'up'
-      yq -r '.services | keys[]' docker-compose*.yml | fzf | tr '\n' ' ' | read services
+      yq -r '.services | keys[]' docker-compose*.yml | sort -u | fzf | tr '\n' ' ' | read services
     end
-    commandline "env COMPOSE_PROJECT_NAME=$project_name docker-compose $argv $services"
-    commandline -f execute
+    set -l cmd "env COMPOSE_PROJECT_NAME=$project_name docker-compose"
+    test -e docker-compose.local.yml && set cmd "$cmd -f docker-compose.yml -f docker-compose.local.yml"
+    commandline "$cmd $argv $services"
 end
--- .config/zsh/custom/docker.zsh
+++ .config/zsh/custom/docker.zsh
@@ -1,7 +1,11 @@
 dc() {
   local services=''
   if [[ "$1" == 'up' ]]; then
-    services="$(yq -r '.services | keys[]' docker-compose*.yml | fzf | tr '\n' ' ')"
+    services="$(yq -r '.services | keys[]' docker-compose*.yml | sort -u | fzf | tr '\n' ' ')"
   fi
-  eval "COMPOSE_PROJECT_NAME=$(current_dir) docker-compose $* $services"
+  local f_option=''
+  if [ -e docker-compose.local.yml ]; then
+    f_option='-f docker-compose.yml -f docker-compose.local.yml'
+  fi
+  eval "COMPOSE_PROJECT_NAME=$(current_dir) docker-compose $f_option $* $services"
 }
