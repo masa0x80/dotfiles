b54890a0a0d6664acc63beeffb633b14fc181af7
KIMURA Masayuki <masa0x80@gmail.com>
2019-07-11 23:39:34 +0900

    [fish] Fix right prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,3 +1,5 @@
 function fish_right_prompt
-    printf (set_color blue)(prompt_pwd)(set_color normal)(__fish_git_prompt)' '(date +%H:%M:%S)
+    echo -n (set_color blue)(prompt_pwd)(set_color normal)
+    echo -n (__fish_git_prompt)
+    echo -n ' '(date +%H:%M:%S)
 end
