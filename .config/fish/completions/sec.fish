b91e2259d1a8f7dc581a9a9b18500e3e2a507bb1
KIMURA Masayuki <masa0x80@gmail.com>
2019-08-01 01:49:21 +0900

    Add `sec` command which is `pass` wrapper

---
+++ .config/fish/completions/sec.fish
@@ -0,0 +1,7 @@
+complete -c sec -d 'Pass command wrapper'
+complete -c sec -n '__fish_use_subcommand' -a list -d 'List up files'
+complete -c sec -n '__fish_use_subcommand' -a backup -d 'Backup the file'
+complete -c sec -n '__fish_use_subcommand' -a show -d 'Print the file on the standard output'
+complete -c sec -n '__fish_use_subcommand' -a diff -d'Compare backup and current files line by line'
+complete -c sec -n '__fish_use_subcommand' -a restore -d 'Restore the file'
+complete -c sec -n '__fish_use_subcommand' -a help -d 'Help'
