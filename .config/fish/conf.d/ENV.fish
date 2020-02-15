290610d4aa4c6366889c8f50e39d2a2a7be7f9c9
KIMURA Masayuki <masa0x80@gmail.com>
2020-02-16 00:46:01 +0900

    [fish] Fix hook config

--- .config/fish/conf.d/ENV.fish
+++ .config/fish/conf.d/ENV.fish
@@ -17,28 +17,6 @@ export fish_color_command=cyan
 export ANGLER_QUERY_OPTION='--no-sort --query'
 # }}}
 
-### Prompt {{{
-
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⨯'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles 'u'
-set __fish_git_prompt_char_stashstate 's'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
-
-# }}}
-
 # Set config path for gabbr
 export gabbr_config=$HOME/.config/fish/gabbr.conf
 
