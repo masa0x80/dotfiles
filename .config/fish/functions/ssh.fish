0b8a21fa6c7bc2e1578260c750d4ed7900fb0638
KIMURA Masayuki <masa0x80@gmail.com>
2020-06-28 22:37:28 +0900

    Remove sshrc

--- .config/fish/functions/ssh.fish
+++
@@ -1,9 +0,0 @@
-function ssh
-    set -l window_name (tmux display -p '#{window_name}')
-    if type -qa sshrc
-        sshrc $argv
-    else
-        command ssh $argv
-    end
-    tmux rename-window $window_name
-end
