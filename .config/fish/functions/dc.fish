8639c7404aa3f388e79edf793eee423ab6f32fc9
KIMURA Masayuki <masa0x80@gmail.com>
2021-01-15 00:23:46 +0900

    [fish] Fix dc function

--- .config/fish/functions/dc.fish
+++ .config/fish/functions/dc.fish
@@ -2,7 +2,7 @@ function dc
     set -l project_name (current_dir -)
     set -l services ''
     if test "$argv[1]" = 'up'
-        set services (yq eval '.services[] | path | .[-1]' docker-compose*.yml | sort -u | fzf | tr '\n' ' ')
+        set services (yq eval -N '.services[] | path | .[-1]' docker-compose*.yml | sort -u | fzf | tr '\n' ' ')
     end
     set -l cmd "env COMPOSE_PROJECT_NAME=$project_name docker-compose"
     test -e docker-compose.local.yml && set cmd "$cmd -f docker-compose.yml -f docker-compose.local.yml"
