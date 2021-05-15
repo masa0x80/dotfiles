b0b09d1d98451973e26e5a9dafa4f4199024452b
KIMURA Masayuki <masa0x80@gmail.com>
2021-05-15 13:45:33 +0900

    Set `$MAKEFLAGS` environment variable
    `bundle --jobs`
    - 以前はUnixWorkerだったが、現在はThreadWorkerになっている
    - IOバウンドには効くが、CPUバウンドには効かない
    
    `MAKE="make --jobs 4" bundle install`
    Native ExtensionのインストールはCPUバウンドなので、
    Makeのjobsオプションを指定する方が効果的。
    
    cf. https://build.betterup.com/one-weird-trick-that-will-speed-up-your-bundle-install/
    
    `$MAKEFLAGS` で指定しておけば常に並列度を指定できる。

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -100,8 +100,8 @@ abbr -a t 'rspec'
 abbr -a ss 'spring stop'
 
 abbr -a be 'bundle exec'
-abbr -a bi "bundle config set path 'vendor/bundle'; bundle binstubs --path=vendor/bin; bundle install --jobs=4"
-abbr -a bil "bundle config set path 'vendor/bundle'; bundle binstubs --path=vendor/bin; bundle install --jobs=4 --local"
+abbr -a bi "bundle config set path 'vendor/bundle'; bundle binstubs --path=vendor/bin; bundle install"
+abbr -a bil "bundle config set path 'vendor/bundle'; bundle binstubs --path=vendor/bin; bundle install --local"
 
 abbr -a solargraph_init "solargraph download-core; solargraph bundle; solargraph config"
 # }}}
