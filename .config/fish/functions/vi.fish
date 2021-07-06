c85ab14df4685af3bcdcb38eb349b133c4a73313
KIMURA Masayuki <masa0x80@gmail.com>
2021-07-07 01:04:20 +0900

    [fish] Fix vi function

--- .config/fish/functions/vi.fish
+++ .config/fish/functions/vi.fish
@@ -1,3 +1,7 @@
 function vi
-    eval $EDITOR (string unescape -- $argv | string escape -n)
+    if type -qa neovide
+        neovide $argv
+    else
+        eval $EDITOR (string unescape -- $argv | string escape -n)
+    end
 end
