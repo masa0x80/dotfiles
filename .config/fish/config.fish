c9d14a826a5a8cc339b87b06386017f832462d3d
KIMURA Masayuki <masa0x80@gmail.com>
2021-11-04 23:39:26 +0900

    [fish] Fix config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,9 +32,11 @@ function __fish_prompt_hooks --on-event fish_prompt
     type -qa direnv && eval (direnv export fish)
 
     # tmux auto rename
-    if status is-interactive && __tmux_is_running && test -z "$VIM_TERMINAL"
-        set -l tmux_current_window_id (tmux list-panes -aF '#{window_id}:#{pane_pid}' | grep :$fish_pid | cut -d: -f1)
-        tmux rename-window -t $tmux_current_window_id (current_dir)(fish_git_prompt || echo '')
+    if test -d .git
+        if status is-interactive && __tmux_is_running && test -z "$VIM_TERMINAL"
+            set -l tmux_current_window_id (tmux list-panes -aF '#{window_id}:#{pane_pid}' | grep :$fish_pid | cut -d: -f1)
+            tmux rename-window -t $tmux_current_window_id (current_dir)(fish_git_prompt || echo '')
+        end
     end
 end
 
