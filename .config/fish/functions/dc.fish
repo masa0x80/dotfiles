94ce1e8ff2a57bb38be0f3b685d16b1e621cb39f
KIMURA Masayuki <masa0x80@gmail.com>
2021-12-15 00:06:21 +0900

    Bugfix: fix return position

--- .config/fish/functions/dc.fish
+++ .config/fish/functions/dc.fish
@@ -1,13 +1,13 @@
 function dc
     set -l project_name (current_dir -)
     set -l services ''
-    if test "$argv[1]" = 'up'
+    if test "$argv[1]" = up
         set services (yq eval -N '.services[] | path | .[-1] | select(. != "app")' docker-compose*.yml | sort -u | fzf | tr '\n' ' ')
+        test "$services" = '' && return
     end
-    test "$services" = '' && return
 
     set -l cmd "env COMPOSE_PROJECT_NAME=$project_name docker compose"
-    test -e docker-compose.yml       && set cmd "$cmd -f docker-compose.yml"
+    test -e docker-compose.yml && set cmd "$cmd -f docker-compose.yml"
     test -e docker-compose.local.yml && set cmd "$cmd -f docker-compose.local.yml"
     commandline "$cmd $argv $services"
     commandline -f execute
