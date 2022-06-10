a806606b80d86acc2a5c8a1132e3dbac691769b0
KIMURA Masayuki <masa0x80@gmail.com>
2022-06-11 00:39:33 +0900

    Fix zsh prompt

--- .config/zsh/hooks/prompt.zsh
+++ .config/zsh/hooks/prompt.zsh
@@ -1,23 +1,17 @@
-setopt nopromptbang prompt{cr,percent,sp,subst}
+source $ZIM_HOME/modules/git-prompt.zsh/git-prompt.zsh
 
-typeset -gA git_info
-if (( ${+functions[git-info]} )); then
-  zstyle ':zim:git-info' verbose yes
-  zstyle ':zim:git-info:action' format '%F{red}%s '
-  zstyle ':zim:git-info:branch' format '%F{yellow} %b'
-  zstyle ':zim:git-info:position' format '%F{magenta}%p'
-  zstyle ':zim:git-info:commit' format '%F{yellow}%c'
-  zstyle ':zim:git-info:stashed' format ' %F{cyan}✭'
-  zstyle ':zim:git-info:indexed' format ' %F{green}✚'
-  zstyle ':zim:git-info:unindexed' format ' %F{blue}✱'
-  zstyle ':zim:git-info:untracked' format ' %F{white}◼'
-  zstyle ':zim:git-info:behind' format ' %F{magenta}⬇'
-  zstyle ':zim:git-info:ahead' format ' %F{magenta}⬆'
-  zstyle ':zim:git-info:keys' format \
-    'status' '%s$(coalesce "%b" "%p" "%c")%S%i%I%u%B%A%f'
+export _PS1='
+%F{blue}%~%f $(gitprompt)%f%(1j.%F{grey}<%j> %f.)%(?,,%F{red}[%?])
+'
 
-  autoload -Uz add-zsh-hook && add-zsh-hook precmd git-info
-fi
-
-export PS1='%F{gray}${(%):-%~} ${(e)git_info[status]}%(?,,%F{red} [%?])
-❯%f '
+_prompt() {
+  counter=$(expr $(expr ${counter:-2} + 1) % 2)
+  local prompt_suffix='匚＞'
+  if [ $counter -eq 0 ]; then
+    prompt_prefix='ミ'
+  else
+    prompt_prefix='彡'
+  fi
+  PS1="${_PS1}%(!.#.%(?,%F{gray}${prompt_prefix}:,%F{red}${prompt_prefix}X)${prompt_suffix})%f "
+}
+add-zsh-hook precmd _prompt
