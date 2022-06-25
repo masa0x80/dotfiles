f57921f2409502e88ed2bac92969607db524aa26
KIMURA Masayuki <masa0x80@gmail.com>
2022-06-25 23:16:58 +0900

    [nvim] Fix spectre config

--- .config/nvim/lua/config/spectre.lua
+++ .config/nvim/lua/config/spectre.lua
@@ -7,7 +7,7 @@ p.setup()
 
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
-keymap("n", "<Leader>S", '<Cmd>lua require("spectre")<CR>', opts)
+keymap("n", "<Leader>S", '<Cmd>lua require("spectre").open()<CR>', opts)
 keymap("n", "<Leader>sw", '<Cmd>lua require("spectre").open_visual({select_word=true})<CR>', opts)
 keymap("v", "<Leader>s", '<Cmd>lua require("spectre").open_visual()<CR>', opts)
 keymap("n", "<Leader>sr", '<Cmd>lua require("spectre").open_file_search()<CR>', opts)
