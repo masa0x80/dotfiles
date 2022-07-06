bc03f4b58858d3bcb56c5784e2503aeeb7fd8af4
KIMURA Masayuki <masa0x80@gmail.com>
2022-07-07 01:02:05 +0900

    [nvim] Fix BackAndForward config

--- .config/nvim/lua/config/BackAndForward.lua
+++ .config/nvim/lua/config/BackAndForward.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<C-g><C-p>", "<Cmd>Back<CR>", opts)
-keymap("n", "<C-g><C-n>", "<Cmd>Forward<CR>", opts)
+keymap("n", "<C-g><C-o>", "<Cmd>Back<CR>", opts)
+keymap("n", "<C-g><C-i>", "<Cmd>Forward<CR>", opts)
