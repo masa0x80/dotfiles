3682a6b35315fe507f40b71e364388ef39515eda
KIMURA Masayuki <masa0x80@gmail.com>
2022-10-12 23:21:36 +0900

    [nvim] Fix git-conflict config

--- .config/nvim/lua/config/git-conflict.lua
+++ .config/nvim/lua/config/git-conflict.lua
@@ -3,4 +3,26 @@ if not status_ok then
 	return
 end
 
-p.setup()
+p.setup({
+	highlights = { -- They must have background color, otherwise the default color will be used
+		incoming = "NormalFloat",
+		current = "NormalFloat",
+	},
+})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd({ "VimEnter", "ColorScheme" }, {
+	group = "_",
+	pattern = "*",
+	command = "highlight GitConflictCurrentLabel guifg=#282c34 guibg=#98c379",
+})
+autocmd({ "VimEnter", "ColorScheme" }, {
+	group = "_",
+	pattern = "*",
+	command = "highlight GitConflictIncomingLabel guifg=#282c34 guibg=#56b6c2",
+})
+autocmd({ "VimEnter", "ColorScheme" }, {
+	group = "_",
+	pattern = "*",
+	command = "highlight GitConflictAncestorLabel guifg=#282c34 guibg=#abb2bf",
+})
