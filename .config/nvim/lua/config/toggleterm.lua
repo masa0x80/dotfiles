cc55a6e0618012d41fab65d63939270b9ad985cb
KIMURA Masayuki <masa0x80@gmail.com>
2022-11-19 16:39:07 +0900

    [nvim] Fix ToggleTerm config

--- .config/nvim/lua/config/toggleterm.lua
+++ .config/nvim/lua/config/toggleterm.lua
@@ -11,3 +11,28 @@ p.setup({
 	},
 	close_on_exit = true,
 })
+
+local keymap = vim.keymap.set
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	keymap("t", "<esc>", [[<C-\><C-n>]], opts)
+	keymap("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	keymap("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	keymap("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	keymap("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	group = "_",
+	pattern = "term://*",
+	command = "lua set_terminal_keymaps()",
+})
+
+local opts = { noremap = true, silent = true }
+keymap("n", ",,f", function()
+	vim.cmd(vim.v.count1 .. "ToggleTerm direction=float")
+end, opts)
+keymap("n", ",,j", function()
+	vim.cmd(vim.v.count1 .. "ToggleTerm direction=horizontal")
+end, opts)
