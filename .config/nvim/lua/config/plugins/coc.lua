ea6c6a97b61b879d2f1d73da6e8b5c08de9db5a3
KIMURA Masayuki <masa0x80@gmail.com>
2022-12-06 01:39:24 +0900

    [nvim] Refine coc config

--- .config/nvim/lua/config/plugins/coc.lua
+++ .config/nvim/lua/config/plugins/coc.lua
@@ -24,74 +24,54 @@ vim.g.coc_global_extensions = {
 	"coc-vimlsp",
 }
 
-vim.cmd([[
-" Use tab for trigger completion with characters ahead and navigate.
-inoremap <silent><expr> <TAB>
-      \ coc#pum#visible() ? coc#_select_confirm() :
-      \ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
-      \ CheckBackSpace() ? "\<TAB>" :
-      \ coc#refresh()
-inoremap <expr> <S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+local keyset = vim.keymap.set
+function _G.check_back_space()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+end
+local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+keyset(
+	"i",
+	"<TAB>",
+	'coc#pum#visible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? \'<C-r>=coc#rpc#request("doKeymap", ["snippets-expand-jump", ""])<CR>\' :  v:lua.check_back_space() ? "<TAB>" : coc#refresh()',
+	opts
+)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
-inoremap <silent><expr> <C-n> coc#pum#visible() ? coc#pum#next(1) : "\<Down>"
-inoremap <silent><expr> <C-p> coc#pum#visible() ? coc#pum#prev(1) : "\<Up>"
+keyset("i", "<C-e>", [[coc#pum#visible() ? coc#pum#cancel() : "\<Esc>A"]], opts)
+keyset("i", "<C-j>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-j>"]], opts)
 
-inoremap <silent><expr> <C-e> coc#pum#visible() ? coc#pum#cancel() : "\<Esc>A"
+keyset("n", "[d", "<Plug>(coc-diagnostic-prev)", { silent = true })
+keyset("n", "]d", "<Plug>(coc-diagnostic-next)", { silent = true })
 
-inoremap <silent><expr> <C-j> coc#pum#visible() ? coc#pum#confirm() : "\<C-j>"
+function _G.show_docs()
+	local cw = vim.fn.expand("<cword>")
+	if vim.fn.index({ "vim", "help" }, vim.bo.filetype) >= 0 then
+		vim.api.nvim_command("h " .. cw)
+	elseif vim.api.nvim_eval("coc#rpc#ready()") then
+		vim.fn.CocActionAsync("doHover")
+	else
+		vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
+	end
+end
+keyset("n", "K", "<CMD>lua _G.show_docs()<CR>", { silent = true })
 
-function! CheckBackSpace() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-endfunction
+vim.api.nvim_create_autocmd("CursorHold", {
+	group = "_",
+	command = "silent call CocActionAsync('highlight')",
+	desc = "Highlight symbol under cursor on CursorHold",
+})
 
-inoremap <silent><expr> <c-space> coc#refresh()
+keyset("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
 
-" Make <CR> to accept selected completion item or notify coc.nvim to format
-" <C-g>u breaks current undo, please make your own choice.
-inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
-                              \: "\<C-g>u\<CR>"
+keyset("n", "<leader>ca", "<Plug>(coc-codeaction)", { silent = true })
+keyset("n", "<leader>cl", "<Plug>(coc-codelens-action)", { silent = true })
 
+local opts = { silent = true, nowait = true }
+keyset("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
+keyset("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
 
-" Use `[d` and `]d` to navigate diagnostics
-" Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-nmap <silent> [d <Plug>(coc-diagnostic-prev)
-nmap <silent> ]d <Plug>(coc-diagnostic-next)
-
-" Use K to show documentation in preview window.
-nnoremap <silent> K :call ShowDocumentation()<CR>
-
-function! ShowDocumentation()
-  if CocAction('hasProvider', 'hover')
-    call CocActionAsync('doHover')
-  else
-    call feedkeys('K', 'in')
-  endif
-endfunction
-
-" Highlight the symbol and its references when holding the cursor.
-autocmd CursorHold * silent call CocActionAsync('highlight')
-
-" Symbol renaming.
-nmap <leader>rn <Plug>(coc-rename)
-
-" Remap keys for applying codeAction to the current buffer.
-nmap <leader>ca  <Plug>(coc-codeaction)
-
-" Run the Code Lens action on the current line.
-nmap <leader>cl  <Plug>(coc-codelens-action)
-
-nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
-inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
-vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-
-" coc-markmap
-" Create markmap from the whole file
-command! Markmap CocCommand markmap.create -w
-]])
+vim.api.nvim_create_user_command("Markmap", "CocCommand mapkmap.create -w", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd({ "VimEnter", "ColorScheme" }, {
