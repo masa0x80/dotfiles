d211751d73c5f1639039cf8daa890c82f5feca42
KIMURA Masayuki <masa0x80@gmail.com>
2022-12-31 21:56:34 +0900

    [nvim] Fix vim-test config

--- .config/nvim/lua/config/plugins/vim-test.lua
+++ .config/nvim/lua/config/plugins/vim-test.lua
@@ -1,28 +1,21 @@
+vim.cmd([[
+  function! MyStrategy(cmd)
+	execute ':TermExec cmd=' .. a:cmd
+  endfunction
+
+  function! JestStrategy(cmd)
+	let testName = matchlist(a:cmd, '\v -t ''(.*)''')[1]
+	call vimspector#LaunchWithSettings( #{ configuration: 'jest', TestName: testName } )
+  endfunction
+
+  let g:test#custom_strategies = {
+	\   'myStrategy': function('MyStrategy'),
+	\   'jest': function('JestStrategy')
+	\ }
+  let g:test#strategy = 'myStrategy'
+]])
+
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
-keymap("n", "<Leader>t", "<Cmd>TestNearest<CR>", opts)
-keymap("n", "<Leader>T", "<Cmd>TestFile<CR>", opts)
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	group = "_",
-	callback = function()
-		vim.cmd([[
-      function! MyNeoVimStrategy(cmd)
-        let term_position = get(g:, 'test#neovim#term_position', 'botright')
-        execute term_position . ' new'
-        call termopen(a:cmd)
-      endfunction
-
-      function! JestStrategy(cmd)
-        let testName = matchlist(a:cmd, '\v -t ''(.*)''')[1]
-        call vimspector#LaunchWithSettings( #{ configuration: 'jest', TestName: testName } )
-      endfunction
-
-      let g:test#custom_strategies = {
-        \   'myneovim': function('MyNeoVimStrategy'),
-        \   'jest': function('JestStrategy')
-        \ }
-      let g:test#strategy = 'myneovim'
-    ]])
-	end,
-})
+keymap("n", "<Leader>t", "<Cmd>TestFile<CR>", opts)
+keymap("n", "<Leader>T", "<Cmd>TestNearest<CR>", opts)
