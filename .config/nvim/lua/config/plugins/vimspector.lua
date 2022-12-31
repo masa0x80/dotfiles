e9cbb138b3294668be96ad2a8bcd3b64d7479a86
KIMURA Masayuki <masa0x80@gmail.com>
2022-12-31 21:56:22 +0900

    [nvim] Fix Vimspector config

--- .config/nvim/lua/config/plugins/vimspector.lua
+++ .config/nvim/lua/config/plugins/vimspector.lua
@@ -4,23 +4,29 @@ local function vimspector_mappings()
 	local opts = { noremap = true, silent = true }
 	local keymap = vim.api.nvim_set_keymap
 
-	keymap("n", "<Leader>ds", "<Cmd>call vimspector#Launch()<CR>", opts)
-	keymap("n", "<Leader>de", "<Cmd>call vimspector#Reset()<CR>", opts)
-	keymap("n", "<Leader>c", "<Cmd>call vimspector#Continue()<CR>", opts)
+	keymap("n", ",dd", "<Cmd>call vimspector#Launch()<CR>", opts)
+	keymap("n", ",de", "<Cmd>call vimspector#Reset()<CR>", opts)
+	keymap("n", ",dr", "<Plug>VimspectorRestart", {})
+	keymap("n", ",c", "<Cmd>call vimspector#Continue()<CR>", opts)
 
-	keymap("n", "<Leader>b", "<Cmd>call vimspector#ToggleBreakpoint()<CR>", opts)
-	keymap("n", "<Leader>B", "<Cmd>call vimspector#ClearBreakpoints()<CR>", opts)
+	keymap("n", ",b", "<Cmd>call vimspector#ToggleBreakpoint()<CR>", opts)
+	keymap("n", ",B", "<Cmd>call vimspector#ClearBreakpoints()<CR>", opts)
 
-	keymap("n", "<Leader>dr", "<Plug>VimspectorRestart", {})
 	keymap("n", ",k", "<Plug>VimspectorStepOut", {})
 	keymap("n", ",l", "<Plug>VimspectorStepInto", {})
 	keymap("n", ",j", "<Plug>VimspectorOvert", {})
-
-	keymap("n", "<Leader>dj", "<Cmd>TestNearest -strategy=jest<CR>", {})
 end
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	group = "_",
 	pattern = { "javascript", "typescript" },
 	callback = vimspector_mappings,
+})
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = "_",
+	pattern = { "javascript", "typescript" },
+	callback = function()
+		local keymap = vim.api.nvim_set_keymap
+		keymap("n", ",dj", "<Cmd>TestNearest -strategy=jest<CR>", {})
+	end,
 })
