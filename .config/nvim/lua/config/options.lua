ee6c15d90bb1c7d947ad5039b1410ede48f13fed
KIMURA Masayuki <masa0x80@gmail.com>
2023-03-01 00:28:05 +0900

    [nvim] Fix foldlevel { 3 => 4 }

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -65,6 +65,6 @@ vim.opt.sessionoptions:append("tabpages,globals")
 -- Folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 3
+vim.opt.foldlevel = 4
 
 vim.opt.includeexpr = "substitute(v:fname, '\\.', '/', 'g')"
