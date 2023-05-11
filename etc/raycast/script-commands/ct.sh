2d3093cb743f1271839a7d1f2bf1e0f4fb6be1aa
KIMURA Masayuki <masa0x80@gmail.com>
2023-05-11 13:23:40 +0900

    [raycast] Fix script-commands

--- etc/raycast/script-commands/ct.sh
+++ etc/raycast/script-commands/ct.sh
@@ -2,7 +2,7 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Copy Current Page URL (Textile) [ct]
+# @raycast.title Copy Current Page URL (Textile) ;ct
 # @raycast.mode silent
 #
 # Optional parameters:
@@ -15,6 +15,6 @@
 
 browser="Vivaldi"
 url=$(osascript -e "tell application \"$browser\" to get URL of active tab of front window")
-title=$(osascript -e "tell application \"$browser\" to get title of active tab of front window")
+title=$(osascript -e "tell application \"$browser\" to get title of active tab of front window" | sed -n 's/ [-|·] [^-|·]*$//p')
 echo "\"$title\":$url" | pbcopy
 echo "Copy Current Page URL (Textile)"
