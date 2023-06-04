3a66d3a701603c0c5f1e804ff6848fc1afe9d71f
KIMURA Masayuki <masa0x80@gmail.com>
2023-06-05 00:26:27 +0900

    [nvim] Tweaks

--- .config/nvim/lua/config/plugins/color/custom.lua
+++ .config/nvim/lua/config/plugins/color/custom.lua
@@ -8,13 +8,15 @@
 --   visual_grey  #3e4452
 --   comment_grey #5c6370
 
-return {
-	Black = "#282c34",
-	Blue = "#61afef",
-	Yellow = "#e5c07b",
-	White = "#aab2bf",
-	Red = "#e06c75",
-	Green = "#98c379",
-	VisualGrey = "#3e4452",
-	CommentGrey = "#5c6370",
-}
+local M = {}
+
+M.Black = "#282c34"
+M.Blue = "#61afef"
+M.Yellow = "#e5c07b"
+M.White = "#aab2bf"
+M.Red = "#e06c75"
+M.Green = "#98c379"
+M.VisualGrey = "#3e4452"
+M.CommentGrey = "#5c6370"
+
+return M
