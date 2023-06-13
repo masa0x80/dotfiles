8b9fa5140fb7a843a23601fa0811d74941246148
KIMURA Masayuki <masa0x80@gmail.com>
2023-06-13 23:08:04 +0900

    [zsh] Tweaks

--- .config/zsh/conf.d/git-branch.zsh
+++ .config/zsh/conf.d/git-branch.zsh
@@ -1,7 +1,7 @@
 git-branch() {
   local option=${1:-'-l'}
-  shift
-  local branch=$(git branch --format='%(refname:short)' --sort=-committerdate $option | fzf +m +s --query="$@")
+  test -n "$1" && shift
+  local branch=$(git branch --format='%(refname:short)' --sort=-committerdate $option | fzf +m +s --query="$LBUFFER")
   if test -n "$branch"; then
     if test "$BUFFER" = ''; then
       BUFFER="git switch"
@@ -9,6 +9,9 @@ git-branch() {
       BUFFER+=" $branch"
       zle end-of-line
       zle accept-line
+      if [ "$?" = 1 ]; then
+        git switch "$(echo $branch | sed -e 's|origin/||')"
+      fi
     else
       BUFFER="${LBUFFER}${branch}${RBUFFER}"
       CURSOR+=${#branch}
@@ -17,17 +20,14 @@ git-branch() {
   fi
 }
 git-switch-local-branch() {
-  git-branch "$@"
+  git-branch
 }
 zle -N git-switch-local-branch
 bindkey "^g^b" git-switch-local-branch
 
 git-switch-remote-branch() {
   git fetch
-  git-branch '-r' "$@"
-  if [ "$?" = 1 ]; then
-    git switch "$(echo $@ | sed -e 's|origin/||')"
-  fi
+  git-branch '-r'
 }
 zle -N git-switch-remote-branch
 bindkey "^g^g^b" git-switch-remote-branch
