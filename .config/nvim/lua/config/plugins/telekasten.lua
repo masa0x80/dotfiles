b45811f1f50480b36c9b2072c33535c076034827
KIMURA Masayuki <masa0x80@gmail.com>
2023-06-21 06:42:07 +0900

    [nvim] Tweaks

--- .config/nvim/lua/config/plugins/telekasten.lua
+++ .config/nvim/lua/config/plugins/telekasten.lua
@@ -62,4 +62,5 @@ keymap("n", ";g", "<Cmd>Telekasten search_notes<CR>", opts)
 
 vim.api.nvim_create_user_command("DeleteTodoMark", function()
 	vim.fn.execute("%s/\\[[x ]\\] //g")
+	vim.fn.execute("nohlsearch")
 end, {})
