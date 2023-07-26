eda66c1153157a868137bcccd52c42b6aa6af2ed
KIMURA Masayuki <masa0x80@gmail.com>
2023-07-27 00:49:49 +0900

    [zsh] Use fzf-tmux

--- .config/zsh/conf.d/git-add.zsh
+++ .config/zsh/conf.d/git-add.zsh
@@ -1,5 +1,5 @@
 git-add() {
-  local files=($(git status --porcelain | fzf -m --query="$@" | cut -c4- | sed -e 's|^|:/|' | tr "\n" ' '))
+  local files=($(git status -s | fzf -m --query="$@" --preview 'git diff --color {-1}' | cut -c4- | sed -e 's|^|:/|' | tr "\n" ' '))
   if test -n "$files"; then
     BUFFER="git add"
     for f in $files; do
--- .config/zsh/lazy/fzf.zsh
+++ .config/zsh/lazy/fzf.zsh
@@ -11,7 +11,8 @@ if (( ${+commands[fd]} )); then
   _fzf_compgen_dir() {
     command fd -c always -H --no-ignore-vcs -E .git -td . "${1}"
   }
-  export FZF_DEFAULT_OPTS='--ansi --reverse --extended --multi --cycle --bind=ctrl-j:accept,ctrl-u:page-up,ctrl-d:page-down,ctrl-g:toggle-all,ctrl-/:deselect-all,ctrl-q:deselect-all'
+  export FZF_DEFAULT_OPTS='--ansi --reverse --extended --multi --cycle --bind=ctrl-j:accept,ctrl-u:page-up,ctrl-d:page-down,ctrl-e:preview-down,ctrl-y:preview-up,ctrl-g:toggle-all,ctrl-/:deselect-all,ctrl-q:deselect-all'
+  export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:-1,bg:#333333,hl:#5f87af --color=fg+:#d0d0d0,bg+:#262626,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
   export FZF_CTRL_T_COMMAND=${FZF_DEFAULT_COMMAND}
 fi
 
@@ -20,3 +21,6 @@ if (( ${+commands[bat]} )); then
 fi
 
 if (( ${+FZF_DEFAULT_COMMAND} )) export FZF_CTRL_T_COMMAND=${FZF_DEFAULT_COMMAND}
+
+export FZF_TMUX_OPTS="-p 80% --border none"
+alias fzf="fzf-tmux $FZF_TMUX_OPTS --ansi"
