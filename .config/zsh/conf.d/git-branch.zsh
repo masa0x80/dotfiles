07ab45bf7f646a6d9d4e752ed4eab6a18c4b085e
KIMURA Masayuki <masa0x80@gmail.com>
2023-08-02 00:02:38 +0900

    [zsh] Fix git scripts

--- .config/zsh/conf.d/git-branch.zsh
+++ .config/zsh/conf.d/git-branch.zsh
@@ -1,5 +1,5 @@
 git-switch-local-branch() {
-  local branch=$(git branch --format='%(refname:short)' --sort=-committerdate -l | fzf +m +s --preview 'git log --graph --format="%C(auto)%h%d %s %C(blue)%cr %C(green)%cN"')
+  local branch=$(git branch --format='%(refname:short)' --sort=-committerdate -l | fzf +m +s --preview 'git log --graph {1} --format="%C(auto)%h%d %s %C(blue)%cr %C(green)%cN"')
   if test -n "$branch"; then
     if test "$BUFFER" = ''; then
       BUFFER="git switch $branch"
@@ -17,7 +17,7 @@ bindkey "^g^b" git-switch-local-branch
 
 git-switch-remote-branch() {
   git fetch
-  local branch=$(git branch --format='%(refname:short)' --sort=-committerdate -r | grep -Ev '^origin$' | fzf +m +s --query="$BUFFER" | sed -e 's|origin/||')
+  local branch=$(git branch --format='%(refname:short)' --sort=-committerdate -r | grep -Ev '^origin$' | fzf +m +s --query="$BUFFER" --preview 'git log --graph {1} --format="%C(auto)%h%d %s %C(blue)%cr %C(green)%cN"' | sed -e 's|origin/||')
   if test -n "$branch"; then
     git branch --format='%(refname:short)' --sort=-committerdate -l | grep -E -q "^${branch}$"
     if [ "$?" = 1 ]; then
