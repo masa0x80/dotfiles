ceaeea863216600fe2dd899be4e031d35065ba7f
KIMURA Masayuki <masa0x80@gmail.com>
2023-08-30 23:11:50 +0900

    [zsh] Fix history-fzf

--- .config/zsh/conf.d/fzf.zsh
+++ .config/zsh/conf.d/fzf.zsh
@@ -71,7 +71,7 @@ if (( ${+commands[fzf]} )); then
   # ref: http://qiita.com/uchiko/items/f6b1528d7362c9310da0
   history-fzf() {
     BUFFER=$(\history -n 1 | \
-      fzf +m --tac --query "$LBUFFER")
+      fzf +m --no-sort --tac --query "$LBUFFER")
     CURSOR=$#BUFFER
     [ -z "$BUFFER" ] && return 0
     zle clear-screen
