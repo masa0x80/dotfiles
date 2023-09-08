485ab7ff82ed43bead21c336b65641d4373db448
KIMURA Masayuki <masa0x80@gmail.com>
2023-09-09 00:37:13 +0900

    [nvim] Add color config

--- .config/nvim/lua/config/color.lua
+++ .config/nvim/lua/config/color.lua
@@ -1,8 +1,11 @@
 -- OneDark
 --   black        #282c34
 --   blue         #61afef
+--   cyan         #56b6c2
+--   purple       #c678dd
 --   yellow       #e5c07b
 --   red          #e06c75
+--   orange       #d19a66
 --   white        #aab2bf
 --   green        #98c379
 --   visual_grey  #3e4452
@@ -12,9 +15,12 @@ local M = {}
 
 M.Black = "#282c34"
 M.Blue = "#61afef"
+M.Cyan = "#56b6c2"
+M.Purple = "#c678dd"
 M.Yellow = "#e5c07b"
 M.White = "#aab2bf"
 M.Red = "#e06c75"
+M.Orange = "#d19a66"
 M.Green = "#98c379"
 M.VisualGrey = "#3e4452"
 M.CommentGrey = "#5c6370"
