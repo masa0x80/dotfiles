9d33bb24b59cbe3e01956f49b35a398ff0847164
KIMURA Masayuki <masa0x80@gmail.com>
2023-09-24 20:13:41 +0900

    [nvim] Fix neo-tree keybind

--- .config/nvim/lua/config/plugins/neo-tree.lua
+++ .config/nvim/lua/config/plugins/neo-tree.lua
@@ -7,14 +7,14 @@ require("neo-tree").setup({
 		git_status = {
 			symbols = {
 				-- Change type
-				added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-				modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-				deleted = "✖", -- this can only be used in the git_status source
-				renamed = "➜", -- this can only be used in the git_status source
+				added = "✚",
+				modified = "",
+				deleted = "✖",
+				renamed = "➜",
 				-- Status type
 				untracked = "",
-				ignored = "◌",
-				unstaged = "",
+				ignored = "",
+				unstaged = "󰄱",
 				staged = "",
 				conflict = "",
 			},
@@ -24,47 +24,51 @@ require("neo-tree").setup({
 			folder_empty_open = "",
 		},
 	},
+	window = {
+		mappings = {
+			["l"] = "open",
+			["<CR>"] = "open",
+			["<BS>"] = "close_node",
+			["h"] = "close_node",
+			["s"] = "open_split",
+			["-"] = "open_split",
+			["v"] = "open_vsplit",
+			["|"] = "open_vsplit",
+			["t"] = "open_tabnew",
+			["i"] = {
+				"add",
+				config = {
+					show_path = "relative",
+				},
+			},
+			["o"] = "add_directory",
+			["d"] = "delete",
+			["r"] = "rename",
+			["c"] = "copy",
+			["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+			["q"] = "close_window",
+			["R"] = "refresh",
+			["?"] = "show_help",
+			["C"] = "close_all_nodes",
+			["<A-p>"] = { "toggle_preview", config = { use_float = true } },
+			["<C-w><C-w>"] = "focus_preview",
+			["<C-c><C-c>"] = "cancel",
+			["<C-;>"] = "preview",
+		},
+	},
 	filesystem = {
 		window = {
 			mappings = {
 				["u"] = "navigate_up",
-				["l"] = "open",
-				["<CR>"] = "open",
-				["<BS>"] = "close_node",
-				["h"] = "close_node",
 				["H"] = "toggle_hidden",
-				["s"] = "open_split",
-				["-"] = "open_split",
-				["v"] = "open_vsplit",
-				["|"] = "open_vsplit",
-				["t"] = "open_tabnew",
 				["f"] = "filter_on_submit",
 				["<C-]>"] = "set_root",
 				["<Esc>"] = "clear_filter",
-				["i"] = {
-					"add",
-					config = {
-						show_path = "relative",
-					},
-				},
-				["o"] = "add_directory",
-				["d"] = "delete",
-				["r"] = "rename",
-				["c"] = "copy",
-				["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
-				["q"] = "close_window",
-				["R"] = "refresh",
-				["?"] = "show_help",
 				["[g"] = "prev_git_modified",
 				["]g"] = "next_git_modified",
-				["C"] = "close_all_nodes",
 				["yy"] = "copy_filename",
 				["Y"] = "copy_relative_filename",
 				["<C-y>"] = "copy_absolute_filename",
-				["<A-p>"] = { "toggle_preview", config = { use_float = true } },
-				["<C-w><C-w>"] = "focus_preview",
-				["<C-c><C-c>"] = "cancel",
-				["<C-;>"] = "preview",
 			},
 		},
 		commands = {
@@ -107,10 +111,37 @@ require("neo-tree").setup({
 		},
 		use_libuv_file_watcher = true,
 	},
+	git_status = {
+		window = {
+			mappings = {
+				["A"] = "git_add_all",
+				["gu"] = "git_unstage_file",
+				["ga"] = "git_add_file",
+				["gr"] = "git_revert_file",
+				["gc"] = "git_commit",
+				["gp"] = "git_push",
+				["gg"] = "git_commit_and_push",
+				["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+				["oc"] = { "order_by_created", nowait = false },
+				["od"] = { "order_by_diagnostics", nowait = false },
+				["om"] = { "order_by_modified", nowait = false },
+				["on"] = { "order_by_name", nowait = false },
+				["os"] = { "order_by_size", nowait = false },
+				["ot"] = { "order_by_type", nowait = false },
+			},
+		},
+	},
 })
 
 local keymap = vim.keymap.set
 keymap("n", "-", "<Cmd>Neotree float reveal_force_cwd<CR>", { noremap = true, silent = true, desc = "NeoTree float" })
+keymap(
+	"n",
+	"<Leader>st",
+	"<Cmd>Neotree float git_status<CR>",
+	{ noremap = true, silent = true, desc = "NeoTree git_status" }
+)
+
 keymap(
 	"n",
 	"<Leader>E",
