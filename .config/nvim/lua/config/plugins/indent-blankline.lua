634effcdc3ed232303f95b3133d69a23c1bd4b9d
KIMURA Masayuki <masa0x80@gmail.com>
2023-10-01 01:38:56 +0900

    [nvim] Fix indent-blankline config

--- .config/nvim/lua/config/plugins/indent-blankline.lua
+++ .config/nvim/lua/config/plugins/indent-blankline.lua
@@ -1,7 +1,1 @@
-require("indent_blankline").setup({
-	char = "¦",
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-	show_trailing_blankline_indent = false,
-})
+require("ibl").setup()
