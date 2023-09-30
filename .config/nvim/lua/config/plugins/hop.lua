3b02bd7cfab5490a4a60a4dc41944cb41d2c7cc6
KIMURA Masayuki <masa0x80@gmail.com>
2023-10-01 01:16:45 +0900

    [nvim] Fix hop config

--- .config/nvim/lua/config/plugins/hop.lua
+++ .config/nvim/lua/config/plugins/hop.lua
@@ -3,20 +3,6 @@ hop.setup({
 	uppercase_labels = true,
 })
 
-local directions = require("hop.hint").HintDirection
-vim.keymap.set("", "f", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set("", "F", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set("", "t", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, { remap = true })
-vim.keymap.set("", "T", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, { remap = true })
-
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
-keymap("n", "<Leader><Space>", ":HopWord<CR>", opts)
+keymap("n", "<Leader><Space>", hop.hint_words, opts)
