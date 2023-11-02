35a7a9d59eae683d946e4167ee511c1d4ae2ac4f
KIMURA Masayuki <masa0x80@gmail.com>
2023-11-02 23:48:59 +0900

    [nvim] Change fold-preview load event { VeryLazy => CursorHold }

--- .config/nvim/lua/plugins/fold-preview.lua
+++ .config/nvim/lua/plugins/fold-preview.lua
@@ -1,6 +1,7 @@
 return {
 	"anuvyklack/fold-preview.nvim",
-	event = "VeryLazy",
+	-- NOTE: VeryLazyだと初期化がうまく行かない場合がある
+	event = "CursorHold",
 	config = require("config.utils").load("conf/fold-preview"),
 	dependencies = {
 		"anuvyklack/pretty-fold.nvim",
