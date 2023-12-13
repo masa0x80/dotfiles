a5037557c3f4a69dcb00c29abc816b5d9bffba59
KIMURA Masayuki <masa0x80@gmail.com>
2023-12-14 00:58:05 +0900

    🩹 Tweaks

--- .config/nvim/lua/plugins/init/telekasten.lua
+++ .config/nvim/lua/plugins/init/telekasten.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_user_command("DeleteTodoMark", function(o)
 	if o.range == 0 then
 		vim.fn.execute("%s/\\[[x ]\\] //g")
 	else
-		vim.fn.execute(o.line1 .. "," .. o.line2 .. "s/\\[[x ]\\] //g")
+		vim.fn.execute(o.line1 .. "," .. o.line2 .. "s/- \\[[x -]\\] /- /g")
 	end
 	vim.fn.execute("nohlsearch")
 end, {
