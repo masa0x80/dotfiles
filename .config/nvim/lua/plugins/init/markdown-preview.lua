79f43c4b8ffb25fd48ea544ab93fcaf85ddb6dc0
KIMURA Masayuki <masa0x80@gmail.com>
2024-01-21 23:57:30 +0900

    [nvim] ✏️ fix: markdown-preview config

--- .config/nvim/lua/plugins/init/markdown-preview.lua
+++ .config/nvim/lua/plugins/init/markdown-preview.lua
@@ -2,7 +2,7 @@ vim.g.mkdp_filetypes = { "markdown", "mermaid", "plantuml" }
 vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/markdown-preview/markdown.css")
 vim.g.mkdp_preview_options = {
 	uml = {
-		server = "http://localhost:8765",
+		server = "http://127.0.0.1:8765",
 		imageFormat = "svg",
 	},
 }
