994e7fcaa529d3fbce29ba0b49cd730d059df69d
KIMURA Masayuki <masa0x80@gmail.com>
2024-01-26 00:57:54 +0900

    [raycast] ✏️ fix: add-bookamark

--- etc/raycast/script-commands/add-bookamark.sh
+++ etc/raycast/script-commands/add-bookamark.sh
@@ -18,9 +18,12 @@ CURRENT_DIR=$(
 )
 source "$CURRENT_DIR/utils/_fetch_url_and_page_title"
 
-FILE="$SCRAPBOOK_DIR/bookmarks.md"
-mkdir -p "$(dirname $FILE)"
-test -e "$FILE" || touch "$FILE"
-
-echo "[$trimmedTitle]($url)" >> "$FILE"
-echo "Added bookmark"
+: "${TARGET_BROWSER:=Safari}"
+url=$(osascript -e "tell application \"$browser\" to get URL of active tab of front window")
+osascript -e "activate application \"$TARGET_BROWSER\""
+osascript -e "delay 0.1"
+osascript -e "tell application \"$TARGET_BROWSER\" to open location \"$url\""
+osascript -e "delay 1"
+osascript -e "activate application \"$TARGET_BROWSER\""
+# Chromiumでは Cmd + d でブックマークできる
+osascript -e "tell application \"System Events\" to keystroke \"d\" using command down"
