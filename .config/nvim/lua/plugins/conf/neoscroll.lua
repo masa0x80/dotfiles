32a0c3f3c09dcbc98f49255681c72a9530349ec1
KIMURA Masayuki <masa0x80@gmail.com>
2024-02-19 17:36:45 +0900

    [nvim] 🩹 Tweaks

--- .config/nvim/lua/plugins/conf/neoscroll.lua
+++ .config/nvim/lua/plugins/conf/neoscroll.lua
@@ -1,14 +1,14 @@
 require("neoscroll").setup({})
 
 local t = {}
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "125" } }
-t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "125" } }
-t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "225" } }
-t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "225" } }
-t["<C-y>"] = { "scroll", { "-0.10", "false", "50" } }
-t["<C-e>"] = { "scroll", { "0.10", "false", "50" } }
-t["zt"] = { "zt", { "125" } }
-t["zz"] = { "zz", { "125" } }
-t["zb"] = { "zb", { "125" } }
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "128" } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "128" } }
+t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "256" } }
+t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "256" } }
+t["<C-y>"] = { "scroll", { "-3", "false", "64" } }
+t["<C-e>"] = { "scroll", { "3", "false", "64" } }
+t["zt"] = { "zt", { "64" } }
+t["zz"] = { "zz", { "64" } }
+t["zb"] = { "zb", { "64" } }
 
 require("neoscroll.config").set_mappings(t)
