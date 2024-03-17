fa597a634c9d33a4bde8b534794e50c127efc88c
KIMURA Masayuki <masa0x80@gmail.com>
2024-03-17 20:26:01 +0900

    [wezterm] 🩹 Tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,13 +10,29 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font_with_fallback({
-	"Moralerspace Neon HWNF",
+config.font = wezterm.font("Moralerspace Argon HWNF", {
+	stretch = "Normal",
+	weight = "Regular",
+	bold = false,
+	italic = false,
 })
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.text_background_opacity = 0.95
+config.font_size = 14.0
+config.adjust_window_size_when_changing_font_size = true
+config.cell_width = 1.0
+config.line_height = 1.0
+config.use_cap_height_to_scale_fallback_fonts = true
+
+-- This increases color saturation by 50%
+config.foreground_text_hsb = {
+	hue = 1.0,
+	saturation = 1.0,
+	brightness = 1.2,
+}
+
 config.color_scheme = "OneDark (base16)"
 config.window_decorations = "RESIZE"
-config.font_size = 14.0
-config.adjust_window_size_when_changing_font_size = false
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.tab_max_width = 64
