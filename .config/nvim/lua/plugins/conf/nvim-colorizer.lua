8c14b47fbb4ee9f8900455a5a1e109b4ad10c7b8
KIMURA Masayuki <masa0x80@gmail.com>
2024-03-31 02:08:41 +0900

    [nvim] ✏️ fix: nvim-colorizer config

--- .config/nvim/lua/plugins/conf/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/conf/nvim-colorizer.lua
@@ -1,1 +1,11 @@
-require("colorizer").setup()
+require("colorizer").setup({ "*" }, {
+	RGB = true,
+	RRGGBB = true,
+	names = true,
+	RRGGBBAA = true,
+	rgb_fn = true,
+	hsl_fn = true,
+	css = true,
+	css_fn = true,
+	mode = "background",
+})
