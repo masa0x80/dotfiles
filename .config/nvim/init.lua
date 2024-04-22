91725d42779b82cb7b0b33979fc22bef940ef4bf
KIMURA Masayuki <masa0x80@gmail.com>
2024-04-23 00:39:53 +0900

    [nvim] 🎸 feat: load local config if exists

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,11 @@
 require("config")
 require("misc")
+
+local load_if_exists = function(path)
+	local p = string.format(vim.fn.expand("$HOME") .. "/.config.local/nvim/lua/%s", path)
+	if vim.fn.filereadable(p) == 1 then
+		vim.fn.execute("luafile " .. p)
+	end
+end
+load_if_exists("config/local.lua")
+load_if_exists("misc/local.lua")
