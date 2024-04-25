c5149a23aee0376c238e091ac3e4e8d4b46e42f1
KIMURA Masayuki <masa0x80@gmail.com>
2024-04-25 23:04:25 +0900

    [nvim] ✏️ fix: markdown-preview config

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,18 +1,1 @@
-local path = vim.fn.expand("$DOTFILE/etc/plantuml/compose.yaml")
-vim.api.nvim_create_user_command("StartPlantUmlServer", function()
-	local msg = vim.fn.system("docker compose -f " .. path .. " up -d")
-	if vim.v.shell_error ~= 0 then
-		vim.notify(vim.fn.substitute(msg, "\n$", "", ""), "error", { render = "default", timeout = 10000 })
-	end
-end, {})
-
-vim.api.nvim_create_user_command("StopPlantUmlServer", function()
-	vim.fn.jobstart("docker compose -f " .. path .. " down")
-end, {})
-
-vim.api.nvim_create_user_command("MarkdownPreviewWrapper", function()
-	vim.fn.execute("StartPlantUmlServer")
-	vim.fn.execute("MarkdownPreview")
-end, {})
-
 vim.opt_local.foldlevel = 1
--- .config/nvim/lua/misc/markdown.lua
+++ .config/nvim/lua/misc/markdown.lua
@@ -10,3 +10,20 @@ if vim.fn.executable("markdown2confluence") == 1 then
 		vim.fn.execute("%y")
 	end, {})
 end
+
+local path = vim.fn.expand("$DOTFILE/etc/plantuml/compose.yaml")
+vim.api.nvim_create_user_command("StartPlantUmlServer", function()
+	local msg = vim.fn.system("docker compose -f " .. path .. " up -d")
+	if vim.v.shell_error ~= 0 then
+		vim.notify(vim.fn.substitute(msg, "\n$", "", ""), "error", { render = "default", timeout = 10000 })
+	end
+end, {})
+
+vim.api.nvim_create_user_command("StopPlantUmlServer", function()
+	vim.fn.jobstart("docker compose -f " .. path .. " down")
+end, {})
+
+vim.api.nvim_create_user_command("MarkdownPreviewWrapper", function()
+	vim.fn.execute("StartPlantUmlServer")
+	vim.fn.execute("MarkdownPreview")
+end, {})
