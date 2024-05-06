27663283a9bfda4318575e7d0c2559e100c6f465
KIMURA Masayuki <masa0x80@gmail.com>
2024-05-07 00:52:39 +0900

    [nvim] ➖ Remove eyeliner

--- .config/nvim/lua/plugins/conf/eyeliner.lua
+++
@@ -1,7 +0,0 @@
-require("eyeliner").setup({
-	highlight_on_key = false,
-})
-
-local c = require("config.color")
-vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = c.fg, bg = c.bg_d, bold = true })
-vim.api.nvim_set_hl(0, "EyelinerSecondary", { reverse = true, bold = true, italic = true })
--- .config/nvim/lua/plugins/eyeliner.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"jinh0/eyeliner.nvim",
-	event = "VeryLazy",
-	config = require("config.utils").load("conf/eyeliner"),
-}
