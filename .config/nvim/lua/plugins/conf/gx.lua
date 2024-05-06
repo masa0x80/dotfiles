e43ad60cba98d1ba7e1a6f9aef21f0bcde848220
KIMURA Masayuki <masa0x80@gmail.com>
2024-05-06 21:40:05 +0900

    [nvim] ➕ Add gx

---
+++ .config/nvim/lua/plugins/conf/gx.lua
@@ -0,0 +1,44 @@
+require("gx").setup({
+	handlers = {
+		plugin = true,
+		github = true,
+		brewfile = true,
+		package_json = true,
+		search = true,
+		go = true,
+		jira = {
+			name = "jira",
+			handle = function(mode, line, _)
+				local ticket = require("gx.helper").find(line, mode, "(%u[%u%d]+-%d+)")
+				local url_base = vim.env.JIRA_BASE_URL
+				if url_base ~= nil and ticket and #ticket < 20 then
+					return url_base .. ticket
+				end
+			end,
+		},
+		rust = {
+			name = "rust",
+			filetype = { "toml" },
+			filename = "Cargo.toml",
+			handle = function(mode, line, _)
+				local crate = require("gx.helper").find(line, mode, "(%w+)%s-=%s")
+
+				if crate then
+					return "https://crates.io/crates/" .. crate
+				end
+			end,
+		},
+		markdown_link = {
+			name = "markdown_link",
+			filetype = { "markdown" },
+			handle = function(mode, line, _)
+				local url =
+					require("gx.helper").find(line, mode, "%[[%w%W]+%]%((https?://[a-zA-Z%d_/%%%-%.~@\\+#=?&:]+)%)")
+
+				if url then
+					return url
+				end
+			end,
+		},
+	},
+})
---
+++ .config/nvim/lua/plugins/gx.lua
@@ -0,0 +1,7 @@
+return {
+	"chrishrb/gx.nvim",
+	keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+	cmd = { "Browse" },
+	config = require("config.utils").load("conf/gx"),
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
