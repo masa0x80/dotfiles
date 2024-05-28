e4e4f21a042c23e56dffeda373aff5b3a4fc2102
KIMURA Masayuki <masa0x80@gmail.com>
2024-05-29 00:05:20 +0900

    [nvim] ✏️ fix: markdown headings highlights

---
+++ .config/nvim/after/queries/markdown/highlights.scm
@@ -0,0 +1,10 @@
+; extends
+
+(atx_heading [
+    (atx_h1_marker)
+    (atx_h2_marker)
+    (atx_h3_marker)
+    (atx_h4_marker)
+    (atx_h5_marker)
+    (atx_h6_marker)
+] @comment)
--- .config/nvim/lua/plugins/conf/render-markdown.lua
+++ .config/nvim/lua/plugins/conf/render-markdown.lua
@@ -2,6 +2,17 @@ require("render-markdown").setup({
 	headings = { "󰉫 ", "󰉬  ", "󰉭   ", "󰉮    ", "󰉯     ", "󰉰      " },
 	bullets = { "󱠦" },
 	highlights = {
+		heading = {
+			backgrounds = { "DiffAdd", "DiffChange", "DiffDelete" },
+			foregrounds = {
+				"comment",
+				"markdownH2",
+				"markdownH3",
+				"markdownH4",
+				"markdownH5",
+				"markdownH6",
+			},
+		},
 		bullet = "Comment",
 	},
 })
