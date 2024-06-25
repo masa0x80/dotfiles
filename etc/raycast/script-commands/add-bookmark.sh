1a767bfa0dd3c465c067c2c20f943ea4b5037bef
KIMURA Masayuki <masa0x80@gmail.com>
2024-06-26 00:14:36 +0900

    [raycast] ✏️ fix: add-bookmark script

--- etc/raycast/script-commands/add-bookmark.sh
+++ etc/raycast/script-commands/add-bookmark.sh
@@ -19,19 +19,25 @@ CURRENT_DIR=$(
 )
 source "$CURRENT_DIR/utils/_fetch_url_and_page_title"
 
-DATE=$(date +%Y-%m-%d)
-TIME=$(date +%H:%M:%S)
-FILENAME="${SCRAPBOOK_DIR}/Bookmarks/${DATE}T${TIME}.md"
+: ${OBSIDIAN_BOOKMARKS_DIR:=$SCRAPBOOK_DIR/Bookmarks}
+FILENAME=$(rg -l "^source: ${url:q}\$" $OBSIDIAN_BOOKMARKS_DIR)
+if [ "$?" -eq 0 ]; then
+  echo "ブックマーク済みです"
+else
+  DATE=$(date +%Y-%m-%d)
+  TIME=$(date +%H%M%S)
+  FILENAME="${OBSIDIAN_BOOKMARKS_DIR}/${DATE//-}${TIME}.md"
 
-echo "---" > $FILENAME
-echo "title: $(echo $title | sed -e 's/:/;/g')" >> $FILENAME
-echo "saved: $DATE" >> $FILENAME
-echo "source: $url" >> $FILENAME
-echo "publisher: $(echo $url | cut -d/ -f3)" >> $FILENAME
-echo "read: false" >> $FILENAME
-echo "tags: []" >> $FILENAME
-echo "---" >> $FILENAME
-echo >> $FILENAME
-echo "# [$title]($url) #bookmark" >> $FILENAME
+  echo "---" > $FILENAME
+  echo "title: \"$(echo $title | sed -e 's/"/\\"/g')\"" >> $FILENAME
+  echo "saved: $DATE" >> $FILENAME
+  echo "source: $url" >> $FILENAME
+  echo "publisher: $(echo $url | cut -d/ -f3)" >> $FILENAME
+  echo "read: false" >> $FILENAME
+  echo "tags: []" >> $FILENAME
+  echo "---" >> $FILENAME
+  echo >> $FILENAME
+  echo "# [$title]($url) #bookmark" >> $FILENAME
+fi
 
 open "obsidian://open?vault=${SCRAPBOOK_DIR:t}&file=${FILENAME:t:r}"
