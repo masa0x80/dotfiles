06dcc2e324a9a87d7a67e9eff285eae110e947d3
KIMURA Masayuki <masa0x80@gmail.com>
2024-07-06 01:04:45 +0900

    [nvim] ✏️ tweak render-markdown config

--- .config/nvim/lua/plugins/conf/render-markdown.lua
+++ .config/nvim/lua/plugins/conf/render-markdown.lua
@@ -1,26 +1,17 @@
 require("render-markdown").setup({
+	render_modes = { "n" },
 	headings = { "󰉫 ", "󰉬  ", "󰉭   ", "󰉮    ", "󰉯     ", "󰉰      " },
 	bullets = { "󱠦" },
 	checkbox = {
 		unchecked = "󰄱  ",
 		checked = "󰄵  ",
 	},
-	win_options = {
-		concealcursor = {
-			rendered = "",
-		},
-	},
 	table_style = "normal",
 	highlights = {
 		heading = {
 			backgrounds = { "DiffAdd", "DiffChange", "DiffDelete" },
 			foregrounds = {
-				"normal",
-				"markdownH2",
-				"markdownH3",
-				"markdownH4",
-				"markdownH5",
-				"markdownH6",
+				"Comment",
 			},
 		},
 		bullet = "Comment",
