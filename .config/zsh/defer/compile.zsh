568fec685cbaa58dbe9baa9068316bc2295d46cb
KIMURA Masayuki <masa0x80@gmail.com>
2024-07-07 17:43:33 +0900

    [zsh] 🩹 Tweaks

--- .config/zsh/defer/compile.zsh
+++ .config/zsh/defer/compile.zsh
@@ -4,4 +4,4 @@
   for src in $@; do
     [ ! -f ${src}.zwc -o $src -nt ${src}.zwc ] && zcompile $src
   done
-} $HOME/.zshenv(N) $ZDOTDIR/.zshrc(N) $ZDOTDIR/*.zsh(N) $ZDOTDIR/defer/*.zsh(N)
+} $HOME/.zshenv(N) $ZDOTDIR/.zshrc(N) $ZDOTDIR/*.zsh(N) $ZDOTDIR/defer/*.zsh(N) $HOME/.config.local/zsh/zshenv(N) $HOME/.config.local/zsh/zshrc(N) $HOME/.zshenv.local(N) $HOME/.zshrc.local(N)
