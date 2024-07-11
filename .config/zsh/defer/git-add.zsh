b88a0dc131397768fa95da2c394d00c007f7e5d0
KIMURA Masayuki <masa0x80@gmail.com>
2024-07-11 22:08:31 +0900

    [zsh] ✏️ fix: git-add

--- .config/zsh/defer/git-add.zsh
+++ .config/zsh/defer/git-add.zsh
@@ -1,13 +1,13 @@
 git-add() {
   local files=(
     $(git -c color.status=always status -s | fzf -m --query="$@" \
-      --preview 'F=$(echo {2..} | sed "s|\"\(.*\)\"|\1|"); [[ {1} = "??" ]] && bat --color=always "$F" || [[ {1} = "MM" ]] && git diff --color=always "$F" || [[ {} =~ "^ M" ]] && git diff --color=always "$F" || [[ {} =~ "^D " || {} =~ "^.D" ]] && : || git diff --staged --color=always "$F"' | cut -c4- | sed -e 's|^|:|' | tr "\n" ' ')
+      --preview 'F=$(echo {2..} | sed "s|\"\(.*\)\"|\1|"); [[ {1} = "??" ]] && bat --color=always "$F" || [[ {1} = "MM" ]] && git diff --color=always "$F" || [[ {} =~ "^ M" ]] && git diff --color=always "$F" || [[ {} =~ "^D " || {} =~ "^.D" ]] && : || git diff --staged --color=always "$F"' | cut -c4- | tr "\n" ' ')
   )
 
   if test -n "$files"; then
     BUFFER="git add"
     for f in $files; do
-      BUFFER+=" $f"
+      BUFFER+=" ./$f"
     done
     zle end-of-line
     zle accept-line
