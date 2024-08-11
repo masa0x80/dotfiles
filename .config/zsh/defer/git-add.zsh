5ab62c408a2c5b09aef5d54bf251420717173e87
KIMURA Masayuki <masa0x80@gmail.com>
2024-08-11 21:28:17 +0900

    [zsh] ✏️ fix: support filename containing spaces

--- .config/zsh/defer/git-add.zsh
+++ .config/zsh/defer/git-add.zsh
@@ -1,17 +1,14 @@
 git-add() {
-  local files=(
-    $(git -c color.status=always status -s | fzf -m --query="$@" \
-      --preview 'F=$(echo {2..} | sed "s|\"\(.*\)\"|\1|"); [[ {1} = "??" ]] && bat --color=always "$F" || [[ {1} = "MM" ]] && git diff --color=always "$F" || [[ {} =~ "^ M" ]] && git diff --color=always "$F" || [[ {} =~ "^D " || {} =~ "^.D" ]] && : || git diff --staged --color=always "$F"' | cut -c4- | tr "\n" ' ')
-  )
-
-  if test -n "$files"; then
-    BUFFER="git add"
-    for f in $files; do
+  IFS=$'\n'
+  for f in $(git -c color.status=always status -s | fzf -m --query="$@" \
+    --preview 'F=$(echo {2..} | sed "s|\"\(.*\)\"|\1|"); [[ {1} = "??" ]] && bat --color=always "$F" || [[ {1} = "MM" ]] && git diff --color=always "$F" || [[ {} =~ "^ M" ]] && git diff --color=always "$F" || [[ {} =~ "^D " ]] && git diff --cached -- "$F" || [[ {} =~ "^.D" ]] && : || git diff --staged --color=always "$F"' | cut -c4-); do
+      if [ "$BUFFER" = "" ]; then
+          BUFFER="git add"
+      fi
       BUFFER+=" ./$f"
-    done
-    zle end-of-line
-    zle accept-line
-  fi
+      zle end-of-line
+      zle accept-line
+  done
 }
 zle -N git-add
 bindkey "^g^a" git-add
