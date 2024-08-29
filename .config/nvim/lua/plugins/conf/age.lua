52dca88c21f6c8523849f415f429da4c75519dc7
KIMURA Masayuki <masa0x80@gmail.com>
2024-08-29 23:58:26 +0900

    [nvim] 🩹 Tweaks

--- .config/nvim/lua/plugins/conf/age.lua
+++ .config/nvim/lua/plugins/conf/age.lua
@@ -1,1 +1,8 @@
 require("age_secret").setup()
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+	pattern = "*.age",
+	callback = function()
+		vim.bo.filetype = "markdown"
+	end,
+})
