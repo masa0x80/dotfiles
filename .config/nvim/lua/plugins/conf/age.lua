dc4a37a01829ba3756fa9b4c6babc286a0af7851
KIMURA Masayuki <masa0x80@gmail.com>
2024-09-15 16:46:39 +0900

    [nvim] ✏️ fix: age config

--- .config/nvim/lua/plugins/conf/age.lua
+++ .config/nvim/lua/plugins/conf/age.lua
@@ -1,8 +1,14 @@
 require("age_secret").setup()
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
-	pattern = "*.age",
+	pattern = "*",
 	callback = function()
-		vim.bo.filetype = "markdown"
+		if vim.fn.expand("%:t"):match("%.md%.age$") then
+			vim.bo.filetype = "markdown"
+		elseif vim.fn.expand("%:t"):match("%.json%.age$") then
+			vim.bo.filetype = "json"
+		elseif vim.fn.expand("%:t"):match("%.csv%.age$") then
+			vim.bo.filetype = "csv"
+		end
 	end,
 })
