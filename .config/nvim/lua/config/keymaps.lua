6132f3c28cf73150d2b3a667084b39a009c09d56
KIMURA Masayuki <masa0x80@gmail.com>
2024-10-15 00:12:05 +0900

    [nvim] 🩹 Tweaks

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -160,7 +160,7 @@ map("c", "<C-a>", "<Home>", {})
 map("c", "<C-e>", "<End>", {})
 
 -- Esc
-map({ "n", "i", "v" }, "<C-c>", "<Esc>", opts)
+map({ "n", "i", "v" }, "<C-c>", "<Esc>", { remap = true })
 map("n", "<Esc><Esc>", ":set nopaste<CR>:nohlsearch<CR>:cclose<CR>:lclose<CR>", opts)
 map("n", "<C-c><C-c>", ":set nopaste<CR>:nohlsearch<CR>:cclose<CR>:lclose<CR>", opts)
 
