2b273f032b7847325b4949ec9cb83c7ea4e03a0b
KIMURA Masayuki <masa0x80@gmail.com>
2024-10-14 22:47:20 +0900

    [nvim] 🎸 feat: add cursor.nvim

---
+++ .config/nvim/lua/plugins/cursor.lua
@@ -0,0 +1,22 @@
+return {
+	"rasulomaroff/cursor.nvim",
+	event = "Colorscheme",
+	opts = {
+		cursors = {
+			{
+				mode = { "n" },
+				-- NOTE: blinking times don't affect cursor blinking times,
+				-- ref. https://github.com/rasulomaroff/cursor.nvim?tab=readme-ov-file#terminals
+				blink = 10,
+			},
+			{
+				mode = { "i" },
+				shape = "ver",
+				size = 100,
+				-- NOTE: blinking times don't affect cursor blinking times,
+				-- ref. https://github.com/rasulomaroff/cursor.nvim?tab=readme-ov-file#terminals
+				blink = 10,
+			},
+		},
+	},
+}
