e775c9a13f9ca015ad3ae82c159c70d57ae7f432
KIMURA Masayuki <masa0x80@gmail.com>
2024-10-22 22:15:08 +0900

    [nvim] ✏️ fix: hop config

--- .config/nvim/lua/plugins/conf/hop.lua
+++ .config/nvim/lua/plugins/conf/hop.lua
@@ -1,17 +1,38 @@
-require("hop").setup({
-	uppercase_labels = true,
-})
+require("hop").setup({})
 
 local map = vim.keymap.set
 local opts = { remap = true }
 local hop = require("hop")
-local directions = require("hop.hint").HintDirection
+local direction = require("hop.hint").HintDirection
 map("", "<Leader><Space>", function()
-	hop.hint_words({})
+	hop.hint_words({ current_line_only = true })
 end, opts)
 map("", "<Leader>k", function()
-	hop.hint_words({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR })
+	hop.hint_lines({ direction = direction.BEFORE_CURSOR })
+end, opts)
+map("", "<Leader>K", function()
+	hop.hint_words({ direction = direction.BEFORE_CURSOR })
 end, opts)
 map("", "<Leader>j", function()
-	hop.hint_words({ direction = require("hop.hint").HintDirection.AFTER_CURSOR })
+	hop.hint_lines({ direction = direction.AFTER_CURSOR })
+end, opts)
+map("", "<Leader>J", function()
+	hop.hint_words({ direction = direction.AFTER_CURSOR })
+end, opts)
+
+map("", "<A-f>", "f", { noremap = true })
+map("", "<A-S-f>", "F", { noremap = true })
+map("", "<A-t>", "t", { noremap = true })
+map("", "<A-S-t>", "T", { noremap = true })
+map("", "f", function()
+	hop.hint_char1({ direction = direction.AFTER_CURSOR, current_line_only = true })
+end, opts)
+map("", "F", function()
+	hop.hint_char1({ direction = direction.BEFORE_CURSOR, current_line_only = true })
+end, opts)
+map("", "t", function()
+	hop.hint_char1({ direction = direction.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, opts)
+map("", "T", function()
+	hop.hint_char1({ direction = direction.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })
 end, opts)
