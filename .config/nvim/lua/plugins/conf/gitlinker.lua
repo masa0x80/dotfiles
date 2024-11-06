677138239fc0835f47e6073bf9a0a1f99d1254a8
KIMURA Masayuki <masa0x80@gmail.com>
2024-11-07 08:37:43 +0900

    [nvim] 🩹 Tweaks

--- .config/nvim/lua/plugins/conf/gitlinker.lua
+++ .config/nvim/lua/plugins/conf/gitlinker.lua
@@ -20,3 +20,6 @@ map(
 vim.api.nvim_create_user_command("GhOpenRepo", function()
 	require("gitlinker").get_repo_url({ action_callback = require("gitlinker.actions").open_in_browser })
 end, {})
+vim.api.nvim_create_user_command("GhOpenFile", function()
+	require("gitlinker").get_buf_range_url("n", { action_callback = require("gitlinker.actions").open_in_browser })
+end, {})
