52a0b0798d463ab2c154cd16a568dcccee7620cb
KIMURA Masayuki <masa0x80@gmail.com>
2025-01-11 00:20:47 +0900

    [nvim] ✏️ fix: img-clip config

--- .config/nvim/lua/plugins/img-clip.lua
+++ .config/nvim/lua/plugins/img-clip.lua
@@ -1,7 +1,24 @@
 return {
 	"HakonHarnes/img-clip.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		default = {
+			relative_to_current_file = true,
+			drag_and_drop = {
+				enabled = true,
+				insert_mode = true,
+			},
+		},
+		filetypes = {
+			markdown = {
+				url_encode_path = false,
+				template = function(context)
+					local path = string.gsub(context.file_path, "/ ", "%%20")
+					return "![" .. context.cursor .. "](" .. path .. ")"
+				end,
+			},
+		},
+	},
 	keys = {
 		{ "<Leader>p", "<Cmd>PasteImage<CR>", desc = "Paste image from system clipboard" },
 	},
