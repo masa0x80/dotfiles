23f449d3cb745d95a4cabb28c0c3e49c7744f39e
KIMURA Masayuki <masa0x80@gmail.com>
2025-02-11 11:07:14 +0900

    [nvim] 🎸 feat: split part of keymaps.lua into commands.lua

---
+++ .config/nvim/lua/config/commands.lua
@@ -0,0 +1,77 @@
+-- JSON.stringify
+vim.api.nvim_create_user_command("JsonStringify", function(args)
+	local cmd
+	if args.range == 2 then
+		cmd = args.line1 .. "," .. args.line2 .. "!xargs -0 -I {} node -e 'console.log(JSON.stringify({}, null, 2))'"
+	else
+		cmd = "%!xargs -0 -I {} node -e 'console.log(JSON.stringify({}, null, 2))'"
+	end
+	vim.fn.execute(cmd)
+end, {
+	range = 2,
+})
+
+-- JSON.parse
+vim.api.nvim_create_user_command("JsonParse", function(args)
+	local cmd
+	if args.range == 2 then
+		cmd = args.line1 .. "," .. args.line2 .. "!xargs -0 -I {} node -e 'console.log({})'"
+	else
+		cmd = "%!xargs -0 -I {} node -e 'console.log({})'"
+	end
+	vim.fn.execute(cmd)
+end, {
+	range = 2,
+})
+
+-- Jq
+vim.api.nvim_create_user_command("Jq", function(opts)
+	local cmd
+	if opts.range == 2 then
+		cmd = opts.line1 .. "," .. opts.line2 .. "!jq ."
+	else
+		cmd = "%!jq ."
+	end
+	vim.fn.execute(cmd)
+end, {
+	range = 2,
+})
+vim.api.nvim_create_user_command("JqCompact", function(opts)
+	local cmd
+	if opts.range == 2 then
+		cmd = opts.line1 .. "," .. opts.line2 .. "!jq -c ."
+	else
+		cmd = "%!jq -c ."
+	end
+	vim.fn.execute(cmd)
+end, {
+	range = 2,
+})
+
+vim.api.nvim_create_user_command("F", function(opts)
+	local ft = opts.fargs[1]
+	if ft ~= nil then
+		vim.bo.filetype = ft
+	else
+		ft = string.match(vim.fn.expand("%:t"), "%.(%w+)%.%w+")
+		if ft ~= nil then
+			if ft == "md" then
+				vim.bo.filetype = "markdown"
+			else
+				vim.bo.filetype = ft
+			end
+			vim.fn.execute("TSBufEnable " .. vim.bo.filetype)
+		end
+	end
+end, {
+	nargs = "?",
+})
+
+vim.api.nvim_create_user_command("AddAbbrComma", function(opts)
+	local k = opts.fargs[1]
+	local v = opts.fargs[2]
+	local cmd = "iabbr " .. k .. " " .. v .. ":<Space>"
+	vim.fn.execute(cmd)
+end, {
+	nargs = "*",
+})
