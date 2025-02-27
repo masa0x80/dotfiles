257d8a14712af8b2e2ce359656e39cf753e9c648
KIMURA Masayuki <masa0x80@gmail.com>
2025-02-27 12:57:58 +0900

    [nvim] 🎸 feat: update blink-cmp config

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -7,17 +7,39 @@ return {
 			return vim.bo.filetype ~= "prompt" and vim.b.completion ~= false
 		end,
 		opts = {
-			cmdline = { enabled = false },
-			keymap = { preset = "enter" },
+			cmdline = {
+				keymap = {
+					preset = "super-tab",
+					["<CR>"] = { "accept_and_enter", "fallback" },
+				},
+			},
+			keymap = {
+				preset = "super-tab",
+				["<CR>"] = { "accept", "fallback" },
+			},
 			completion = {
-				documentation = { auto_show = true, auto_show_delay_ms = 200 },
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+					window = { border = "rounded" },
+				},
+				menu = { border = "rounded" },
 			},
 			appearance = {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
 			},
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				default = { "snippets", "lsp", "path", "buffer" },
+				-- NOTE: Neovim補完プラグインblink.cmpの使い方とカスタマイズ https://eiji.page/blog/neovim-blink-cmp-intro/
+				min_keyword_length = function(ctx)
+					-- :wq, :qa -> menu doesn't popup
+					-- :Lazy, :wqa -> menu popup
+					if ctx.mode == "cmdline" and ctx.line:find("^%l+$") ~= nil then
+						return 3
+					end
+					return 0
+				end,
 			},
 		},
 		snippets = { preset = "luasnip" },
