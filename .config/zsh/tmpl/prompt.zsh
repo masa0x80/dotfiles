d10f095481c8f3becc7a1dd6d8d1852ad882044e
KIMURA Masayuki <masa0x80@gmail.com>
2025-03-12 23:19:06 +0900

    [zsh] 🎸 feat: add starship

--- .config/zsh/tmpl/prompt.zsh
+++ .config/zsh/tmpl/prompt.zsh
@@ -1,11 +1,9 @@
-_prompt() {
+_switch_starship_config() {
   counter=$(expr $(expr ${counter:-1} + 1) % 2)
-  local prompt_suffix='匚＞'
   if [ $counter -eq 0 ]; then
-    prompt_prefix='ミ'
+    export STARSHIP_CONFIG=$HOME/.config/starship.toml
   else
-    prompt_prefix='彡'
+    export STARSHIP_CONFIG=$HOME/.config/starship2.toml
   fi
-  PURE_PROMPT_SYMBOL="%(!.#.%(?,%F{magenta}${prompt_prefix}:,%F{red}${prompt_prefix}X)${prompt_suffix})%f"
 }
-add-zsh-hook precmd _prompt
+add-zsh-hook precmd _switch_starship_config
