eb94785ab6644dcc11f56e8b701688b497c54ac8
KIMURA Masayuki <masa0x80@gmail.com>
2025-03-13 01:21:13 +0900

    [nvim] 🎸 feat: update blink.cmp config

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -30,7 +30,13 @@ return {
 				nerd_font_variant = "mono",
 			},
 			sources = {
-				default = { "buffer", "lsp", "snippets", "path" },
+				default = function()
+					if vim.bo.filetype == "markdown" then
+						return { "lsp", "snippets", "path" }
+					else
+						return { "buffer", "lsp", "snippets", "path" }
+					end
+				end,
 				-- NOTE: Neovim補完プラグインblink.cmpの使い方とカスタマイズ https://eiji.page/blog/neovim-blink-cmp-intro/
 				min_keyword_length = function(ctx)
 					-- :wq, :qa -> menu doesn't popup
