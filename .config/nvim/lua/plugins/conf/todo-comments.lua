5403d95d6afba950ed710c62b0873502a5bceede
KIMURA Masayuki <masa0x80@gmail.com>
2025-03-15 22:41:24 +0900

    [nvim] 🎸 feat: add todo-comments.nvim

--- .config/nvim/lua/plugins/conf/todo-comments.lua
+++
@@ -1,7 +0,0 @@
-require("todo-comments").setup({
-	highlight = {
-		pattern = [[.*<(KEYWORDS)\s*:?]],
-	},
-})
-
-vim.keymap.set("n", "<Leader>V", "<Cmd>TodoTelescope<CR>", { noremap = true, silent = true, desc = "TodoTelescope" })
--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,5 +1,53 @@
 return {
 	"folke/todo-comments.nvim",
-	event = "VeryLazy",
-	config = require("config.utils").load("conf/todo-comments"),
+	opts = {
+		highlight = {
+			pattern = [[.*<(KEYWORDS):?\s]],
+		},
+		search = {
+			command = "rg",
+			args = {
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--hidden",
+			},
+			pattern = [[\b(KEYWORDS):]],
+		},
+	},
+	keys = {
+		{
+			"]c",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next Todo Comment",
+		},
+		{
+			"[c",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Previous Todo Comment",
+		},
+		{
+			"<Leader>st",
+			function()
+				Snacks.picker.todo_comments({ hidden = true })
+			end,
+			desc = "Todo",
+		},
+		{
+			"<Leader>sT",
+			function()
+				Snacks.picker.todo_comments({
+					keywords = { "TODO", "FIX", "FIXME", "XXX" },
+					hidden = true,
+				})
+			end,
+			desc = "Todo/Fix/Fixme",
+		},
+	},
 }
