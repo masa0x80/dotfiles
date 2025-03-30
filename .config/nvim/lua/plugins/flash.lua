75569d3cccaf28653de69193642b2e491834b6cc
KIMURA Masayuki <masa0x80@gmail.com>
2025-03-31 00:31:43 +0900

    [nvim] 🩹 Tweaks flash config

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -3,13 +3,26 @@ return {
 	version = "*",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		labels = "fjdksla;ghtyrueiwoqpbnvmc,x.z/",
+	},
 	-- stylua: ignore
 	keys = {
 		{ "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash", },
 		{ "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
 		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash", },
 		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search", },
-		{ "<Leader><Space>", mode = { "n", "o", "x" }, function() require("flash").jump({ search = { mode = "search", max_length = 0 }, label = { after = { 0, 0 } }, pattern = "^", }) end, desc = "Jump to a line" },
+		{
+			"<Leader><Space>",
+			mode = { "n", "o", "x" },
+			function()
+				require("flash").jump({
+				  search = { mode = "search", max_length = 0 },
+				  label = { after = false, before = {0, 0} },
+				  pattern = "^",
+				})
+			end,
+			desc = "Jump to a line"
+		},
 	},
 }
