2ad2b73bbc7473d16f98f0e4a09540eb88038e9e
KIMURA Masayuki <masa0x80@gmail.com>
2025-04-23 01:39:38 +0900

    [nvim] ➕ Add lspkind

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -52,7 +52,45 @@ return {
 					auto_show_delay_ms = 200,
 					window = { border = "rounded" },
 				},
-				menu = { border = "rounded" },
+				menu = {
+					border = "rounded",
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local lspkind = require("lspkind")
+									local icon = ctx.kind_icon
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											icon = dev_icon
+										end
+									elseif ctx.kind == "Copilot" then
+										icon = ""
+									else
+										icon = lspkind.symbolic(ctx.kind)
+									end
+
+									return icon .. ctx.icon_gap
+								end,
+
+								-- Optionally, use the highlight groups from nvim-web-devicons
+								-- You can also add the same function for `kind.highlight` if you want to
+								-- keep the highlight groups in sync with the icons.
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+									return hl
+								end,
+							},
+						},
+					},
+				},
 			},
 			appearance = {
 				use_nvim_cmp_as_default = true,
