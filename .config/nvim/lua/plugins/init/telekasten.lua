d6afac1f4131131f72c19bb406d6e4816dd55fd7
KIMURA Masayuki <masa0x80@gmail.com>
2025-08-04 20:39:30 +0900

    [nvim] ✏️ fix DeleteTodoMark

--- .config/nvim/lua/plugins/init/telekasten.lua
+++ .config/nvim/lua/plugins/init/telekasten.lua
@@ -1,10 +1,26 @@
 vim.api.nvim_create_user_command("DeleteTodoMark", function(o)
 	if o.range == 0 then
-		vim.fn.execute("%s/- \\[[x -]\\] [0-9][0-9]:[0-9][0-9] - [0-9][0-9]:[0-9][0-9] /- /g")
+		pcall(function()
+			vim.fn.execute("%s/- \\[[x -]\\] [0-9][0-9]:[0-9][0-9] - [0-9][0-9]:[0-9][0-9] /- /g")
+		end)
+		pcall(function()
+			vim.fn.execute("%s/- \\[[x -]\\] <.*> /- /g")
+		end)
+		pcall(function()
+			vim.fn.execute("%s/- \\[[x -]\\] /- /g")
+		end)
 	else
-		vim.fn.execute(
-			o.line1 .. "," .. o.line2 .. "s/- \\[[x -]\\] [0-9][0-9]:[0-9][0-9] - [0-9][0-9]:[0-9][0-9] /- /g"
-		)
+		pcall(function()
+			vim.fn.execute(
+				o.line1 .. "," .. o.line2 .. "s/- \\[[x -]\\] [0-9][0-9]:[0-9][0-9] - [0-9][0-9]:[0-9][0-9] /- /g"
+			)
+		end)
+		pcall(function()
+			vim.fn.execute(o.line1 .. "," .. o.line2 .. "s/- \\[[x -]\\] <.*> /- /g")
+		end)
+		pcall(function()
+			vim.fn.execute(o.line1 .. "," .. o.line2 .. "s/- \\[[x -]\\] /- /g")
+		end)
 	end
 	vim.fn.execute("nohlsearch")
 end, {
