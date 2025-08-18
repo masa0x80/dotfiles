62618f5f498ee4ce3c4b085fb54539947fa29973
KIMURA Masayuki <masa0x80@gmail.com>
2025-08-18 23:40:20 +0900

    [raycast] 🎸 feat: archive.is

---
+++ etc/raycast/script-commands/archive.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Web Archive
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName 魚拓
+# @raycast.icon 📜
+#
+# @raycast.author KIMURA Masayuki
+
+CURRENT_DIR=$(
+  cd "$(dirname "$0")" || exit
+  pwd
+)
+source "$CURRENT_DIR/utils/_fetch_url_and_page_title"
+
+open "https://archive.is/$url"
