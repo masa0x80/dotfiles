33b6eb7685439d1d4184498851e0ecb23099bfc0
KIMURA Masayuki <masa0x80@gmail.com>
2025-09-05 22:44:31 +0900

    [nvim] 🩹 Tweaks

--- .config/nvim/lua/plugins/init/markdown-preview.lua
+++ .config/nvim/lua/plugins/init/markdown-preview.lua
@@ -1,5 +1,5 @@
 vim.g.mkdp_filetypes = { "markdown", "mermaid", "plantuml" }
-vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/markdown-preview/markdown.css")
+vim.g.mkdp_theme = "light"
 local default_options = {
 	disable_sync_scroll = true,
 	uml = {
