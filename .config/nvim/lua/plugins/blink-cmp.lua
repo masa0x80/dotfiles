9c2791a5003a214e8cd2af8c6324b250a8c1831e
KIMURA Masayuki <masa0x80@gmail.com>
2026-01-19 21:54:30 +0900

    ✏️ [nvim] fix blink-cmp config

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -41,9 +41,6 @@ return {
 					function() -- sidekick next edit suggestion
 						return require("sidekick").nes_jump_or_apply()
 					end,
-					function() -- if you are using Neovim's native inline completions
-						return vim.lsp.inline_completion.get()
-					end,
 					"fallback",
 				},
 				["<C-CR>"] = { "show", "show_documentation", "hide_documentation" },
