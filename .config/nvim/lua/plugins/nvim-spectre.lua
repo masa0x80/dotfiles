7ff0f4c2f4dc993098475eecd8ad139b13e3dbc9
KIMURA Masayuki <masa0x80@gmail.com>
2026-04-09 00:24:38 +0900

    🩹 [nvim] Tweaks

--- .config/nvim/lua/plugins/nvim-spectre.lua
+++ .config/nvim/lua/plugins/nvim-spectre.lua
@@ -2,4 +2,16 @@ return {
 	"nvim-pack/nvim-spectre",
 	version = "*",
 	event = "VeryLazy",
+	highlight = {
+		ui = "String",
+		search = "DiffChange",
+		replace = "DiffAdd",
+	},
+	mapping = {
+		["run_replace"] = {
+			map = "<leader><C-r>",
+			cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+			desc = "replace all",
+		},
+	},
 }
