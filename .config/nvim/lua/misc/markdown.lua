a1c75f6875b9f235fdb3559e6cf8e66a507dc7c9
KIMURA Masayuki <masa0x80@gmail.com>
2026-04-10 23:28:44 +0900

    🩹 [nvim] Tweaks

--- .config/nvim/lua/misc/markdown.lua
+++ .config/nvim/lua/misc/markdown.lua
@@ -1,64 +1,78 @@
-local path = vim.fn.expand("$DOTFILE/etc/plantuml/compose.yaml")
-vim.api.nvim_create_user_command("StartPlantUmlServer", function()
-	vim.system({ "docker", "compose", "-f", path, "up", "-d" }, { text = true }, function(obj)
+local compose_path = vim.fn.expand("$DOTFILE/etc/plantuml/compose.yaml")
+local tmp_dir = "/tmp/plantuml-viewer/"
+local port = 8765
+
+local function start_plantuml_server()
+	vim.system({ "docker", "compose", "-f", compose_path, "up", "-d" }, { text = true }, function(obj)
 		if obj.code ~= 0 then
 			vim.notify(tostring(obj.code), "error", { render = "default", timeout = 10000 })
 		end
 	end)
-end, {})
-
-vim.api.nvim_create_user_command("StopPlantUmlServer", function()
-	vim.system({ "docker", "compose", "-f", path, "down" })
-end, {})
-
-vim.api.nvim_create_user_command("MarkdownPreviewWrapper", function()
-	vim.fn.execute("StartPlantUmlServer")
-	vim.fn.execute("MarkdownPreview")
-end, {})
-
-local tmp_dir = "/tmp/plantuml-viewer/"
-local port = "8766"
-local function writeSvg()
-	local filename = vim.fn.expand("%:t")
-	vim.fn.jobstart(
-		"curl -H 'Content-Type: application/text; charset=UTF-8' --data-binary @"
-			.. vim.fn.expand("%")
-			.. " 'http://127.0.0.1:8765/svg/' > "
-			.. tmp_dir
-			.. "out.svg",
-		{
-			on_exit = function()
-				vim.system({ "cp", tmp_dir .. "out.svg", tmp_dir .. filename .. ".svg" })
-			end,
-		}
-	)
 end
 
-vim.api.nvim_create_user_command("PlantUMLPreview", function()
-	vim.fn.execute("StartPlantUmlServer")
+local function stop_plantuml_server()
+	vim.system({ "docker", "compose", "-f", compose_path, "down" })
+end
+
+local function write_svg()
+	local filepath = vim.fn.expand("%:p")
 	local filename = vim.fn.expand("%:t")
-	vim.system({ "mkdir", "-p", tmp_dir })
-	writeSvg()
-	vim.system({ "cp", vim.fn.expand("$DOTFILE/etc/plantuml/viewer.html"), tmp_dir })
+	local outfile = tmp_dir .. "out.svg"
+	vim.system({
+		"curl",
+		"-s",
+		"-H",
+		"Content-Type: application/text; charset=UTF-8",
+		"--data-binary",
+		"@" .. filepath,
+		("http://localhost:%d/svg/"):format(port),
+	}, { text = true }, function(obj)
+		if obj.code ~= 0 then
+			return
+		end
+		local f = io.open(outfile, "w")
+		if not f then
+			return
+		end
+		f:write(obj.stdout)
+		f:close()
+		vim.uv.fs_copyfile(outfile, tmp_dir .. filename .. ".svg")
+	end)
+end
+
+vim.api.nvim_create_user_command("StartPlantUmlServer", start_plantuml_server, {})
+vim.api.nvim_create_user_command("StopPlantUmlServer", stop_plantuml_server, {})
+
+vim.api.nvim_create_user_command("MarkdownPreviewWrapper", function()
+	start_plantuml_server()
+	vim.cmd("MarkdownPreview")
+end, {})
+vim.api.nvim_create_user_command("PlantUMLPreview", function()
+	start_plantuml_server()
+	local filename = vim.fn.expand("%:t")
+	vim.fn.mkdir(tmp_dir, "p")
+	write_svg()
+	vim.uv.fs_copyfile(vim.fn.expand("$DOTFILE/etc/plantuml/viewer.html"), tmp_dir .. "viewer.html")
 	-- 前回起動したプロセスが残っていたらkill
-	vim.system({ "pkill", "python", "-m", "http.server", port, "--directory", tmp_dir }):wait()
-	vim.system({ "python", "-m", "http.server", port, "--directory", tmp_dir })
+	local port2 = 8766
+	vim.system({ "pkill", "-f", "python -m http.server " .. port2 }):wait()
+	vim.system({ "python", "-m", "http.server", port2, "--directory", tmp_dir })
 	vim.system({
 		"open",
 		"-n",
 		"-a",
 		vim.fn.expand("$TARGET_BROWSER"),
 		"--args",
-		"http://localhost:" .. port .. "/viewer.html?filename=" .. filename,
+		("http://localhost:%d/viewer.html?filename=%s"):format(port2, filename),
 	})
-	local group = "PlantUMLPreview:" .. filename
-	vim.api.nvim_create_augroup(group, { clear = true })
-	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	local group = vim.api.nvim_create_augroup("PlantUMLPreview:" .. filename, { clear = true })
+	require("utils").create_autocmd({ "BufWritePost" }, {
 		group = group,
 		buffer = vim.fn.bufnr(),
-		callback = writeSvg,
+		callback = write_svg,
 	})
 end, {})
+
 require("utils").create_autocmd({ "FileType" }, {
 	pattern = "markdown",
 	callback = function(args)
