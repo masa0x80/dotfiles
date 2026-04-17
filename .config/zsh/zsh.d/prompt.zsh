a678d8ba5e9f7c81dfe9b5722a7455b7e5b74105
KIMURA Masayuki <masa0x80@gmail.com>
2026-04-18 01:43:48 +0900

    🩹 [zsh] Tweaks

--- .config/zsh/zsh.d/prompt.zsh
+++ .config/zsh/zsh.d/prompt.zsh
@@ -2,7 +2,6 @@ export STARSHIP_CONFIG1="$HOME/.config/starship.toml"
 export STARSHIP_CONFIG2="$HOME/.config/starship2.toml"
 
 _switch_starship_config() {
-  echo -ne "\033]2;$(current_dir)\007"
   if [ "$STARSHIP_CONFIG" = "$STARSHIP_CONFIG1" ]; then
     STARSHIP_CONFIG="$STARSHIP_CONFIG2"
   else
@@ -11,3 +10,8 @@ _switch_starship_config() {
   export STARSHIP_CONFIG
 }
 add-zsh-hook preexec _switch_starship_config
+
+_set_window_name() {
+  echo -ne "\033]2;$(current_dir)\007"
+}
+add-zsh-hook chpwd _set_window_name
