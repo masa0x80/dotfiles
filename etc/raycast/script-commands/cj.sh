19bbf6b514eae49be8bedca8c9907722e019a023
KIMURA Masayuki <masa0x80@gmail.com>
2026-06-27 14:43:25 +0900

    🩹 [raycast] Tweaks script-commands to support multiple browsers

--- etc/raycast/script-commands/cj.sh
+++ etc/raycast/script-commands/cj.sh
@@ -13,11 +13,13 @@
 # @raycast.author KIMURA Masayuki
 #
 # @raycast.argument1 { "type": "dropdown", "data": [{"title": "trimmedTitle", "value": "trim"}, {"title": "rawTitle", "value": "raw"}], "placeholder": "trimmedTitle", "optional": true }
+# @raycast.argument2 { "type": "dropdown", "data": [{"title": "Brave", "value": "Brave Browser"}, {"title": "Chrome", "value": "Google Chrome"}, {"title": "Edge", "value": "Microsoft Edge"}, {"title": "Safari", "value": "Safari"}, {"title": "Vivaldi", "value": "Vivaldi"}], "placeholder": "Safari", "optional": true }
 
 CURRENT_DIR=$(
   cd "$(dirname "$0")" || exit
   pwd
 )
+TARGET_BROWSER="${2:-$TARGET_BROWSER}"
 source "$CURRENT_DIR/utils/_fetch_url_and_page_title"
 
 if [ "$1" = "raw" ]; then
